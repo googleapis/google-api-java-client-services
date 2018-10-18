b239ed434efbba5e7bd037f351f0bf8386ed9ec6
dpebot <dpebot@google.com>
2018-10-18 03:35:50 -0700

    Regenerate appengine client

--- clients/1.26.0/google-api-services-appengine/v1/com/google/appengine/v1/model/Version.java
+++ clients/1.26.0/google-api-services-appengine/v1/com/google/appengine/v1/model/Version.java
@@ -155,6 +155,12 @@ public final class Version extends com.google.api.client.json.GenericJson {
   @com.google.api.client.util.Key
   private java.util.List<UrlMap> handlers;
 
+  static {
+    // hack to force ProGuard to consider UrlMap used, since otherwise it would be stripped out
+    // see https://github.com/google/google-api-java-client/issues/543
+    com.google.api.client.util.Data.nullOf(UrlMap.class);
+  }
+
   /**
    * Configures health checking for instances. Unhealthy instances are stopped and replaced with new
    * instances. Only applicable in the App Engine flexible environment.Only returned in GET requests

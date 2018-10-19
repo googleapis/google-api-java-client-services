7a193e2bf1fc9ddf09e15b2b86658367c0112db4
Jeff Ching <chingor@google.com>
2018-10-19 09:53:11 -0700

    Use batch mode to make output more readable

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -30,7 +30,7 @@ deploy_library() {
   LIBRARY_VERSION=$5
   echo "Releasing artifact for ${SERVICE}, ${API_VERSION}, ${REVISION}, ${LIBRARY_VERSION}."
 
-  mvn clean install
+  mvn clean install -B
   mvn deploy \
     --settings ${MAVEN_SETTINGS_FILE} \
     -DperformRelease=true \

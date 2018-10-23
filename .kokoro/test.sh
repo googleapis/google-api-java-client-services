8cb88cb83a39a8b4814b0539f0611b177fc152eb
Jeff Ching <chingor@google.com>
2018-10-23 16:13:04 -0700

    Add Kokoro CI (#99)
    * Add presubmit job that builds, tests, and packages each artifact
    
    * Only run tests if something has changed in that directory
    
    * Capture output into sponge_log.log files
    
    * Upload sponge_log.log files as artifacts
    
    * Fix working directory
    
    * Put the test change back
    
    * Put the test pom back

---
+++ .kokoro/test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+EXIT_STATUS=0
+
+pushd $(dirname "$0")/../
+
+for directory in `find clients -mindepth 3 -maxdepth 3 -type d | sort`
+do
+  pushd $directory
+  diff=$(git diff master .)
+  if [ -z "$diff" ]; then
+    # skipping tests
+    echo "No difference from master, skipping tests."
+  else
+    mvn clean verify package -B | tee sponge_log.log
+    es=$?
+    if [ $es -ne 0 ]; then
+        EXIT_STATUS=$es
+    fi
+  fi
+  popd
+done
+
+popd
+
+exit $EXIT_STATUS

8ee6248763c9b39dbafa1be32920e5c733336a84
yoshi-automation <yoshi-automation@google.com>
2018-11-11 03:50:00 -0800

    Regenerate servicemanagement client

--- clients/1.26.0/google-api-services-servicemanagement/v1/com/google/api/services/servicemanagement/model/GenerateConfigReportResponse.java
+++ clients/1.26.0/google-api-services-servicemanagement/v1/com/google/api/services/servicemanagement/model/GenerateConfigReportResponse.java
@@ -37,12 +37,6 @@ public final class GenerateConfigReportResponse extends com.google.api.client.js
   @com.google.api.client.util.Key
   private java.util.List<ChangeReport> changeReports;
 
-  static {
-    // hack to force ProGuard to consider ChangeReport used, since otherwise it would be stripped out
-    // see https://github.com/google/google-api-java-client/issues/543
-    com.google.api.client.util.Data.nullOf(ChangeReport.class);
-  }
-
   /**
    * Errors / Linter warnings associated with the service definition this report belongs to.
    * The value may be {@code null}.

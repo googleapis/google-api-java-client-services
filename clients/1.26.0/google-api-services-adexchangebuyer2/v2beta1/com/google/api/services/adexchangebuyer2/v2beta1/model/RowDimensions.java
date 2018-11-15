6dd8f4aa2c95ae861998beedfdb509a436032dd2
yoshi-automation <yoshi-automation@google.com>
2018-11-15 03:34:06 -0800

    Regenerate adexchangebuyer client

--- clients/1.26.0/google-api-services-adexchangebuyer2/v2beta1/com/google/api/services/adexchangebuyer2/v2beta1/model/RowDimensions.java
+++ clients/1.26.0/google-api-services-adexchangebuyer2/v2beta1/com/google/api/services/adexchangebuyer2/v2beta1/model/RowDimensions.java
@@ -33,7 +33,9 @@ public final class RowDimensions extends com.google.api.client.json.GenericJson 
 
   /**
    * The publisher identifier for this row, if a breakdown by
-   * BreakdownDimension.PUBLISHER_IDENTIFIER was requested.
+   * [BreakdownDimension.PUBLISHER_IDENTIFIER](https://developers.google.com/authorized-
+   * buyers/apis/reference/rest/v2beta1/bidders.accounts.filterSets#FilterSet.BreakdownDimension)
+   * was requested.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -48,7 +50,9 @@ public final class RowDimensions extends com.google.api.client.json.GenericJson 
 
   /**
    * The publisher identifier for this row, if a breakdown by
-   * BreakdownDimension.PUBLISHER_IDENTIFIER was requested.
+   * [BreakdownDimension.PUBLISHER_IDENTIFIER](https://developers.google.com/authorized-
+   * buyers/apis/reference/rest/v2beta1/bidders.accounts.filterSets#FilterSet.BreakdownDimension)
+   * was requested.
    * @return value or {@code null} for none
    */
   public java.lang.String getPublisherIdentifier() {
@@ -57,7 +61,9 @@ public final class RowDimensions extends com.google.api.client.json.GenericJson 
 
   /**
    * The publisher identifier for this row, if a breakdown by
-   * BreakdownDimension.PUBLISHER_IDENTIFIER was requested.
+   * [BreakdownDimension.PUBLISHER_IDENTIFIER](https://developers.google.com/authorized-
+   * buyers/apis/reference/rest/v2beta1/bidders.accounts.filterSets#FilterSet.BreakdownDimension)
+   * was requested.
    * @param publisherIdentifier publisherIdentifier or {@code null} for none
    */
   public RowDimensions setPublisherIdentifier(java.lang.String publisherIdentifier) {

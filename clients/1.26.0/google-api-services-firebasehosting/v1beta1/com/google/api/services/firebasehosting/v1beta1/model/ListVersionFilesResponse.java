8440d55a0351c0f3bb80fa2cfd6fe7b8f8b6e925
yoshi-automation <yoshi-automation@google.com>
2018-11-17 03:50:37 -0800

    Regenerate firebasehosting client

--- clients/1.26.0/google-api-services-firebasehosting/v1beta1/com/google/api/services/firebasehosting/v1beta1/model/ListVersionFilesResponse.java
+++ clients/1.26.0/google-api-services-firebasehosting/v1beta1/com/google/api/services/firebasehosting/v1beta1/model/ListVersionFilesResponse.java
@@ -36,6 +36,12 @@ public final class ListVersionFilesResponse extends com.google.api.client.json.G
   @com.google.api.client.util.Key
   private java.util.List<VersionFile> files;
 
+  static {
+    // hack to force ProGuard to consider VersionFile used, since otherwise it would be stripped out
+    // see https://github.com/google/google-api-java-client/issues/543
+    com.google.api.client.util.Data.nullOf(VersionFile.class);
+  }
+
   /**
    * The pagination token, if more results exist.
    * The value may be {@code null}.

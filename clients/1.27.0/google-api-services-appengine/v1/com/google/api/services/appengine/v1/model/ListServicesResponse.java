813f6be084d29846b81d603932a29b154b99baf5
yoshi-automation <yoshi-automation@google.com>
2018-11-18 03:36:35 -0800

    Regenerate appengine client

--- clients/1.27.0/google-api-services-appengine/v1/com/google/api/services/appengine/v1/model/ListServicesResponse.java
+++ clients/1.27.0/google-api-services-appengine/v1/com/google/api/services/appengine/v1/model/ListServicesResponse.java
@@ -43,12 +43,6 @@ public final class ListServicesResponse extends com.google.api.client.json.Gener
   @com.google.api.client.util.Key
   private java.util.List<Service> services;
 
-  static {
-    // hack to force ProGuard to consider Service used, since otherwise it would be stripped out
-    // see https://github.com/google/google-api-java-client/issues/543
-    com.google.api.client.util.Data.nullOf(Service.class);
-  }
-
   /**
    * Continuation token for fetching the next page of results.
    * @return value or {@code null} for none

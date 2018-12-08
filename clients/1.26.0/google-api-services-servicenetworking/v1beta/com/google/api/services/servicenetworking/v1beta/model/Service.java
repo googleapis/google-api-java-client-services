353d1586715dc449e286ffab5d52388296990545
yoshi-automation <yoshi-automation@google.com>
2018-12-08 03:56:50 -0800

    Regenerate servicenetworking client

--- clients/1.26.0/google-api-services-servicenetworking/v1beta/com/google/api/services/servicenetworking/v1beta/model/Service.java
+++ clients/1.26.0/google-api-services-servicenetworking/v1beta/com/google/api/services/servicenetworking/v1beta/model/Service.java
@@ -51,6 +51,12 @@ public final class Service extends com.google.api.client.json.GenericJson {
   @com.google.api.client.util.Key
   private java.util.List<Api> apis;
 
+  static {
+    // hack to force ProGuard to consider Api used, since otherwise it would be stripped out
+    // see https://github.com/google/google-api-java-client/issues/543
+    com.google.api.client.util.Data.nullOf(Api.class);
+  }
+
   /**
    * Auth configuration.
    * The value may be {@code null}.

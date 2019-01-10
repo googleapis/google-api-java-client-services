41064e7ec772a52943b4f87c0c2074e845dc0be6
Jeff Ching <chingor@google.com>
2019-01-10 15:56:57 -0800

    Add Kokoro script for running all the tests

---
+++ .kokoro/generator_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+EXIT_STATUS=0
+
+pushd $(dirname "$0")/..
+python2 -m pip install -e generator/ --user
+
+pushd generator
+for file in `find src/ -name '*_test.py'`
+do
+  python2 $file
+  es=$?
+  if [ $es -ne 0 ]; then
+      EXIT_STATUS=$es
+  fi
+done
+popd
+
+popd
+
+exit $EXIT_STATUS

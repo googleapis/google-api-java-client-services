9a8787fdf7a26d1f6b385922dd1234b07e501ef5
yoshi-automation <yoshi-automation@google.com>
2019-03-27 04:15:42 -0700

    Regenerate speech client

--- clients/1.28.0/google-api-services-speech/v1p1beta1/com/google/api/services/speech/v1p1beta1/Speech.java
+++ clients/1.28.0/google-api-services-speech/v1p1beta1/com/google/api/services/speech/v1p1beta1/Speech.java
@@ -1080,7 +1080,8 @@ public class Speech extends com.google.api.client.googleapis.services.json.Abstr
     /**
      * Performs asynchronous speech recognition: receive results via the google.longrunning.Operations
      * interface. Returns either an `Operation.error` or an `Operation.response` which contains a
-     * `LongRunningRecognizeResponse` message.
+     * `LongRunningRecognizeResponse` message. For more information on asynchronous speech recognition,
+     * see the [how-to](https://cloud.google.com/speech-to-text/docs/async-recognize).
      *
      * Create a request for the method "speech.longrunningrecognize".
      *
@@ -1104,7 +1105,8 @@ public class Speech extends com.google.api.client.googleapis.services.json.Abstr
       /**
        * Performs asynchronous speech recognition: receive results via the google.longrunning.Operations
        * interface. Returns either an `Operation.error` or an `Operation.response` which contains a
-       * `LongRunningRecognizeResponse` message.
+       * `LongRunningRecognizeResponse` message. For more information on asynchronous speech
+       * recognition, see the [how-to](https://cloud.google.com/speech-to-text/docs/async-recognize).
        *
        * Create a request for the method "speech.longrunningrecognize".
        *

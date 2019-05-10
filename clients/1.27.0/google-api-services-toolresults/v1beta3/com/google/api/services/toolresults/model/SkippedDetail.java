d539bf6874a5bbb9d94c62516c2df014fb74f891
yoshi-automation <yoshi-automation@google.com>
2019-05-10 04:17:41 -0700

    Regenerate toolresults client

--- clients/1.27.0/google-api-services-toolresults/v1beta3/com/google/api/services/toolresults/model/SkippedDetail.java
+++ clients/1.27.0/google-api-services-toolresults/v1beta3/com/google/api/services/toolresults/model/SkippedDetail.java
@@ -17,7 +17,7 @@
 package com.google.api.services.toolresults.model;
 
 /**
- * Model definition for SkippedDetail.
+ * Details for an outcome with a SKIPPED outcome summary.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Cloud Tool Results API. For a detailed explanation

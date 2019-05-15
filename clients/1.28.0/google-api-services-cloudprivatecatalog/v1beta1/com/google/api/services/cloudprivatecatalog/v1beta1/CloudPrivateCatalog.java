ba43e5a635ecac140438f8cdf28e77bc5255db2a
yoshi-automation <yoshi-automation@google.com>
2019-05-15 03:44:15 -0700

    Regenerate cloudprivatecatalog client

--- clients/1.28.0/google-api-services-cloudprivatecatalog/v1beta1/com/google/api/services/cloudprivatecatalog/v1beta1/CloudPrivateCatalog.java
+++ clients/1.28.0/google-api-services-cloudprivatecatalog/v1beta1/com/google/api/services/cloudprivatecatalog/v1beta1/CloudPrivateCatalog.java
@@ -25,7 +25,7 @@ package com.google.api.services.cloudprivatecatalog.v1beta1;
  *
  * <p>
  * For more information about this service, see the
- * <a href="https://sites.google.com/corp/google.com/cloudprivatecatalog" target="_blank">API Documentation</a>
+ * <a href="https://cloud.google.com/private-catalog/" target="_blank">API Documentation</a>
  * </p>
  *
  * <p>

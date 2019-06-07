2569c0f37bb9a685a5ab032a11a34fce0b0db037
yoshi-automation <yoshi-automation@google.com>
2019-06-07 04:17:45 -0700

    Regenerate servicenetworking client

--- clients/google-api-services-servicenetworking/v1beta/1.28.0/com/google/api/services/servicenetworking/v1beta/model/MonitoredResourceDescriptor.java
+++ clients/google-api-services-servicenetworking/v1beta/1.28.0/com/google/api/services/servicenetworking/v1beta/model/MonitoredResourceDescriptor.java
@@ -25,6 +25,8 @@ package com.google.api.services.servicenetworking.v1beta.model;
  * Different APIs can support different monitored resource types. APIs generally provide a `list`
  * method that returns the monitored resource descriptors used by the API.
  *
+ * Next ID: 10
+ *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Service Networking API. For a detailed explanation
  * see:

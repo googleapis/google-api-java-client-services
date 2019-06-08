182b01429fbfe1903bf790275c569b25ba5c2d22
yoshi-automation <yoshi-automation@google.com>
2019-06-08 04:07:04 -0700

    Regenerate logging client

--- clients/google-api-services-logging/v2/1.26.0/com/google/api/services/logging/v2/Logging.java
+++ clients/google-api-services-logging/v2/1.26.0/com/google/api/services/logging/v2/Logging.java
@@ -20,7 +20,7 @@ package com.google.api.services.logging.v2;
  * Service definition for Logging (v2).
  *
  * <p>
- * Writes log entries and manages your Logging configuration.
+ * Writes log entries and manages your Stackdriver Logging configuration. The table entries below are presented in alphabetical order, not in order of common use. For explanations of the concepts found in the table entries, read the Stackdriver Logging documentation.
  * </p>
  *
  * <p>

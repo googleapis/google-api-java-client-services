58ad07ec66d54ae5077f1a3a91379baf28bbe0b0
yoshi-automation <yoshi-automation@google.com>
2019-06-09 04:09:39 -0700

    Regenerate redis client

--- clients/google-api-services-redis/v1beta1/1.27.0/com/google/api/services/redis/v1beta1/model/GcsSource.java
+++ clients/google-api-services-redis/v1beta1/1.27.0/com/google/api/services/redis/v1beta1/model/GcsSource.java
@@ -17,7 +17,7 @@
 package com.google.api.services.redis.v1beta1.model;
 
 /**
- * The GCS location for the input content
+ * The Cloud Storage location for the input content
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Google Cloud Memorystore for Redis API. For a

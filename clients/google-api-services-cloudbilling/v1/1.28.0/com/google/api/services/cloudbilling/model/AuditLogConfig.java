db715946c138c06d76f2d00f0e10a6b2a5084ab2
yoshi-automation <yoshi-automation@google.com>
2019-07-09 03:43:48 -0700

    Regenerate cloudbilling client

--- clients/google-api-services-cloudbilling/v1/1.28.0/com/google/api/services/cloudbilling/model/AuditLogConfig.java
+++ clients/google-api-services-cloudbilling/v1/1.28.0/com/google/api/services/cloudbilling/model/AuditLogConfig.java
@@ -45,6 +45,14 @@ public final class AuditLogConfig extends com.google.api.client.json.GenericJson
   private java.util.List<java.lang.String> exemptedMembers;
 
   /**
+   * Specifies whether principals can be exempted for the same LogType in lower-level resource
+   * policies. If true, any lower-level exemptions will be ignored.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Boolean ignoreChildExemptions;
+
+  /**
    * The log type that this config enables.
    * The value may be {@code null}.
    */
@@ -67,6 +75,25 @@ public final class AuditLogConfig extends com.google.api.client.json.GenericJson
    */
   public AuditLogConfig setExemptedMembers(java.util.List<java.lang.String> exemptedMembers) {
     this.exemptedMembers = exemptedMembers;
+    return this;
+  }
+
+  /**
+   * Specifies whether principals can be exempted for the same LogType in lower-level resource
+   * policies. If true, any lower-level exemptions will be ignored.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Boolean getIgnoreChildExemptions() {
+    return ignoreChildExemptions;
+  }
+
+  /**
+   * Specifies whether principals can be exempted for the same LogType in lower-level resource
+   * policies. If true, any lower-level exemptions will be ignored.
+   * @param ignoreChildExemptions ignoreChildExemptions or {@code null} for none
+   */
+  public AuditLogConfig setIgnoreChildExemptions(java.lang.Boolean ignoreChildExemptions) {
+    this.ignoreChildExemptions = ignoreChildExemptions;
     return this;
   }
 

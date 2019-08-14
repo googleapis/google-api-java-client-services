6b0e82c714026d281bc4e36f5ff19949e2865169
yoshi-automation <yoshi-automation@google.com>
2019-08-14 04:33:12 -0700

    Regenerate vision client

--- clients/google-api-services-vision/v1/1.30.1/com/google/api/services/vision/v1/model/SafeSearchAnnotation.java
+++ clients/google-api-services-vision/v1/1.30.1/com/google/api/services/vision/v1/model/SafeSearchAnnotation.java
@@ -39,11 +39,32 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   private java.lang.String adult;
 
   /**
+   * Confidence of adult_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float adultConfidence;
+
+  /**
    * Likelihood that this is a medical image.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
   private java.lang.String medical;
+
+  /**
+   * Confidence of medical_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float medicalConfidence;
+
+  /**
+   * Confidence of nsfw_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float nsfwConfidence;
 
   /**
    * Likelihood that the request image contains racy content. Racy content may include (but is not
@@ -55,6 +76,13 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   private java.lang.String racy;
 
   /**
+   * Confidence of racy_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float racyConfidence;
+
+  /**
    * Spoof likelihood. The likelihood that an modification was made to the image's canonical version
    * to make it appear funny or offensive.
    * The value may be {@code null}.
@@ -63,11 +91,25 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   private java.lang.String spoof;
 
   /**
+   * Confidence of spoof_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float spoofConfidence;
+
+  /**
    * Likelihood that this image contains violent content.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
   private java.lang.String violence;
+
+  /**
+   * Confidence of violence_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.Float violenceConfidence;
 
   /**
    * Represents the adult content likelihood for the image. Adult content may contain elements such
@@ -89,6 +131,23 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   }
 
   /**
+   * Confidence of adult_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getAdultConfidence() {
+    return adultConfidence;
+  }
+
+  /**
+   * Confidence of adult_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param adultConfidence adultConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setAdultConfidence(java.lang.Float adultConfidence) {
+    this.adultConfidence = adultConfidence;
+    return this;
+  }
+
+  /**
    * Likelihood that this is a medical image.
    * @return value or {@code null} for none
    */
@@ -102,6 +161,40 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
    */
   public SafeSearchAnnotation setMedical(java.lang.String medical) {
     this.medical = medical;
+    return this;
+  }
+
+  /**
+   * Confidence of medical_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getMedicalConfidence() {
+    return medicalConfidence;
+  }
+
+  /**
+   * Confidence of medical_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param medicalConfidence medicalConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setMedicalConfidence(java.lang.Float medicalConfidence) {
+    this.medicalConfidence = medicalConfidence;
+    return this;
+  }
+
+  /**
+   * Confidence of nsfw_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getNsfwConfidence() {
+    return nsfwConfidence;
+  }
+
+  /**
+   * Confidence of nsfw_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param nsfwConfidence nsfwConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setNsfwConfidence(java.lang.Float nsfwConfidence) {
+    this.nsfwConfidence = nsfwConfidence;
     return this;
   }
 
@@ -127,6 +220,23 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   }
 
   /**
+   * Confidence of racy_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getRacyConfidence() {
+    return racyConfidence;
+  }
+
+  /**
+   * Confidence of racy_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param racyConfidence racyConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setRacyConfidence(java.lang.Float racyConfidence) {
+    this.racyConfidence = racyConfidence;
+    return this;
+  }
+
+  /**
    * Spoof likelihood. The likelihood that an modification was made to the image's canonical version
    * to make it appear funny or offensive.
    * @return value or {@code null} for none
@@ -146,6 +256,23 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
   }
 
   /**
+   * Confidence of spoof_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getSpoofConfidence() {
+    return spoofConfidence;
+  }
+
+  /**
+   * Confidence of spoof_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param spoofConfidence spoofConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setSpoofConfidence(java.lang.Float spoofConfidence) {
+    this.spoofConfidence = spoofConfidence;
+    return this;
+  }
+
+  /**
    * Likelihood that this image contains violent content.
    * @return value or {@code null} for none
    */
@@ -159,6 +286,23 @@ public final class SafeSearchAnnotation extends com.google.api.client.json.Gener
    */
   public SafeSearchAnnotation setViolence(java.lang.String violence) {
     this.violence = violence;
+    return this;
+  }
+
+  /**
+   * Confidence of violence_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @return value or {@code null} for none
+   */
+  public java.lang.Float getViolenceConfidence() {
+    return violenceConfidence;
+  }
+
+  /**
+   * Confidence of violence_score. Range [0, 1]. 0 means not confident, 1 means very confident.
+   * @param violenceConfidence violenceConfidence or {@code null} for none
+   */
+  public SafeSearchAnnotation setViolenceConfidence(java.lang.Float violenceConfidence) {
+    this.violenceConfidence = violenceConfidence;
     return this;
   }
 

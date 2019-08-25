2cf81f5f4eeeabd0e75c0b337fc835c8a283a5ea
yoshi-automation <yoshi-automation@google.com>
2019-08-25 04:18:04 -0700

    Regenerate tpu client

--- clients/google-api-services-tpu/v1/1.28.0/com/google/api/services/tpu/v1/model/ListAcceleratorTypesResponse.java
+++ clients/google-api-services-tpu/v1/1.28.0/com/google/api/services/tpu/v1/model/ListAcceleratorTypesResponse.java
@@ -50,6 +50,13 @@ public final class ListAcceleratorTypesResponse extends com.google.api.client.js
   private java.lang.String nextPageToken;
 
   /**
+   * Locations that could not be reached.
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.util.List<java.lang.String> unreachable;
+
+  /**
    * The listed nodes.
    * @return value or {@code null} for none
    */
@@ -80,6 +87,23 @@ public final class ListAcceleratorTypesResponse extends com.google.api.client.js
    */
   public ListAcceleratorTypesResponse setNextPageToken(java.lang.String nextPageToken) {
     this.nextPageToken = nextPageToken;
+    return this;
+  }
+
+  /**
+   * Locations that could not be reached.
+   * @return value or {@code null} for none
+   */
+  public java.util.List<java.lang.String> getUnreachable() {
+    return unreachable;
+  }
+
+  /**
+   * Locations that could not be reached.
+   * @param unreachable unreachable or {@code null} for none
+   */
+  public ListAcceleratorTypesResponse setUnreachable(java.util.List<java.lang.String> unreachable) {
+    this.unreachable = unreachable;
     return this;
   }
 

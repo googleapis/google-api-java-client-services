a57ed16930cc84f9320196ac0dd1533ebe86e2d2
yoshi-automation <yoshi-automation@google.com>
2019-09-10 03:42:01 -0700

    Regenerate binaryauthorization client

---
+++ clients/google-api-services-binaryauthorization/v1/README.md
@@ -0,0 +1,45 @@
+# Binary Authorization API Client Library for Java
+
+The management interface for Binary Authorization, a system providing policy control for images deployed to Kubernetes Engine clusters.
+
+
+This page contains information about getting started with the Binary Authorization API
+using the Google API Client Library for Java. In addition, you may be interested
+in the following documentation:
+
+* Browse the [Javadoc reference for the Binary Authorization API][javadoc]
+* Read the [Developer's Guide for the Google API Client Library for Java][google-api-client].
+* Interact with this API in your browser using the [APIs Explorer for the Binary Authorization API][api-explorer]
+
+## Installation
+
+### Maven
+
+Add the following lines to your `pom.xml` file:
+
+```xml
+<project>
+  <dependencies>
+    <dependency>
+      <groupId>com.google.apis</groupId>
+      <artifactId>google-api-services-binaryauthorization</artifactId>
+      <version>v1-rev20190906-1.30.1</version>
+    </dependency>
+  </dependencies>
+</project>
+```
+
+### Gradle
+
+```gradle
+repositories {
+  mavenCentral()
+}
+dependencies {
+  compile 'com.google.apis:google-api-services-binaryauthorization:v1-rev20190906-1.30.1'
+}
+```
+
+[javadoc]: https://googleapis.dev/java/google-api-services-binaryauthorization/latest/index.html
+[google-api-client]: https://github.com/googleapis/google-api-java-client/
+[api-explorer]: https://developers.google.com/apis-explorer/#p/abusiveexperiencereport/v1/
--- clients/google-api-services-binaryauthorization/v1beta1/1.28.0/com/google/api/services/binaryauthorization/v1beta1/model/IamPolicy.java
+++ clients/google-api-services-binaryauthorization/v1beta1/1.28.0/com/google/api/services/binaryauthorization/v1beta1/model/IamPolicy.java
@@ -80,7 +80,12 @@ public final class IamPolicy extends com.google.api.client.json.GenericJson {
   private java.lang.String etag;
 
   /**
-   * Deprecated.
+   * Specifies the format of the policy.
+   *
+   * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
+   *
+   * Policies with any conditional bindings must specify version 3. Policies without any conditional
+   * bindings may specify any valid value or leave the field unset.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -181,7 +186,12 @@ public final class IamPolicy extends com.google.api.client.json.GenericJson {
   }
 
   /**
-   * Deprecated.
+   * Specifies the format of the policy.
+   *
+   * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
+   *
+   * Policies with any conditional bindings must specify version 3. Policies without any conditional
+   * bindings may specify any valid value or leave the field unset.
    * @return value or {@code null} for none
    */
   public java.lang.Integer getVersion() {
@@ -189,7 +199,12 @@ public final class IamPolicy extends com.google.api.client.json.GenericJson {
   }
 
   /**
-   * Deprecated.
+   * Specifies the format of the policy.
+   *
+   * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
+   *
+   * Policies with any conditional bindings must specify version 3. Policies without any conditional
+   * bindings may specify any valid value or leave the field unset.
    * @param version version or {@code null} for none
    */
   public IamPolicy setVersion(java.lang.Integer version) {

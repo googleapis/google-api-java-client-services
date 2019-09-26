6082ca7f9ebdc95d57885ce529649b562a197bed
yoshi-automation <yoshi-automation@google.com>
2019-09-26 04:20:02 -0700

    Regenerate servicebroker client

--- clients/google-api-services-servicebroker/v1/1.29.2/com/google/api/services/servicebroker/v1/model/GoogleIamV1Policy.java
+++ clients/google-api-services-servicebroker/v1/1.29.2/com/google/api/services/servicebroker/v1/model/GoogleIamV1Policy.java
@@ -20,22 +20,31 @@ package com.google.api.services.servicebroker.v1.model;
  * Defines an Identity and Access Management (IAM) policy. It is used to specify access control
  * policies for Cloud Platform resources.
  *
- * A `Policy` consists of a list of `bindings`. A `binding` binds a list of `members` to a `role`,
- * where the members can be user accounts, Google groups, Google domains, and service accounts. A
- * `role` is a named list of permissions defined by IAM.
+ * A `Policy` is a collection of `bindings`. A `binding` binds one or more `members` to a single
+ * `role`. Members can be user accounts, service accounts, Google groups, and domains (such as G
+ * Suite). A `role` is a named list of permissions (defined by IAM or configured by users). A
+ * `binding` can optionally specify a `condition`, which is a logic expression that further
+ * constrains the role binding based on attributes about the request and/or target resource.
  *
  * **JSON Example**
  *
- *     {       "bindings": [         {           "role": "roles/owner",           "members": [
- * "user:mike@example.com",             "group:admins@example.com",             "domain:google.com",
- * "serviceAccount:my-other-app@appspot.gserviceaccount.com"           ]         },         {
- * "role": "roles/viewer",           "members": ["user:sean@example.com"]         }       ]     }
+ *     {       "bindings": [         {           "role": "roles/resourcemanager.organizationAdmin",
+ * "members": [             "user:mike@example.com",             "group:admins@example.com",
+ * "domain:google.com",             "serviceAccount:my-project-id@appspot.gserviceaccount.com"
+ * ]         },         {           "role": "roles/resourcemanager.organizationViewer",
+ * "members": ["user:eve@example.com"],           "condition": {             "title": "expirable
+ * access",             "description": "Does not grant access after Sep 2020",
+ * "expression": "request.time <             timestamp('2020-10-01T00:00:00.000Z')",           }
+ * }       ]     }
  *
  * **YAML Example**
  *
  *     bindings:     - members:       - user:mike@example.com       - group:admins@example.com
- * - domain:google.com       - serviceAccount:my-other-app@appspot.gserviceaccount.com       role:
- * roles/owner     - members:       - user:sean@example.com       role: roles/viewer
+ * - domain:google.com       - serviceAccount:my-project-id@appspot.gserviceaccount.com       role:
+ * roles/resourcemanager.organizationAdmin     - members:       - user:eve@example.com       role:
+ * roles/resourcemanager.organizationViewer       condition:         title: expirable access
+ * description: Does not grant access after Sep 2020         expression: request.time <
+ * timestamp('2020-10-01T00:00:00.000Z')
  *
  * For a description of IAM and its features, see the [IAM developer's
  * guide](https://cloud.google.com/iam/docs).
@@ -51,7 +60,8 @@ package com.google.api.services.servicebroker.v1.model;
 public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJson {
 
   /**
-   * Associates a list of `members` to a `role`. `bindings` with no members will result in an error.
+   * Associates a list of `members` to a `role`. Optionally may specify a `condition` that
+   * determines when binding is in effect. `bindings` with no members will result in an error.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -72,7 +82,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    * to the same version of the policy.
    *
    * If no `etag` is provided in the call to `setIamPolicy`, then the existing policy is
-   * overwritten.
+   * overwritten. Due to blind-set semantics of an etag-less policy, 'setIamPolicy' will not fail
+   * even if either of incoming or stored policy does not meet the version requirements.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -83,15 +94,21 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    *
    * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
    *
-   * Policies with any conditional bindings must specify version 3. Policies without any conditional
-   * bindings may specify any valid value or leave the field unset.
+   * Operations affecting conditional bindings must specify version 3. This can be either setting a
+   * conditional policy, modifying a conditional binding, or removing a conditional binding from the
+   * stored conditional policy. Operations on non-conditional policies may specify any valid value
+   * or leave the field unset.
+   *
+   * If no etag is provided in the call to `setIamPolicy`, any version compliance checks on the
+   * incoming and/or stored policy is skipped.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
   private java.lang.Integer version;
 
   /**
-   * Associates a list of `members` to a `role`. `bindings` with no members will result in an error.
+   * Associates a list of `members` to a `role`. Optionally may specify a `condition` that
+   * determines when binding is in effect. `bindings` with no members will result in an error.
    * @return value or {@code null} for none
    */
   public java.util.List<GoogleIamV1Binding> getBindings() {
@@ -99,7 +116,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
   }
 
   /**
-   * Associates a list of `members` to a `role`. `bindings` with no members will result in an error.
+   * Associates a list of `members` to a `role`. Optionally may specify a `condition` that
+   * determines when binding is in effect. `bindings` with no members will result in an error.
    * @param bindings bindings or {@code null} for none
    */
   public GoogleIamV1Policy setBindings(java.util.List<GoogleIamV1Binding> bindings) {
@@ -116,7 +134,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    * to the same version of the policy.
    *
    * If no `etag` is provided in the call to `setIamPolicy`, then the existing policy is
-   * overwritten.
+   * overwritten. Due to blind-set semantics of an etag-less policy, 'setIamPolicy' will not fail
+   * even if either of incoming or stored policy does not meet the version requirements.
    * @see #decodeEtag()
    * @return value or {@code null} for none
    */
@@ -133,7 +152,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    * to the same version of the policy.
    *
    * If no `etag` is provided in the call to `setIamPolicy`, then the existing policy is
-   * overwritten.
+   * overwritten. Due to blind-set semantics of an etag-less policy, 'setIamPolicy' will not fail
+   * even if either of incoming or stored policy does not meet the version requirements.
    * @see #getEtag()
    * @return Base64 decoded value or {@code null} for none
    *
@@ -152,7 +172,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    * to the same version of the policy.
    *
    * If no `etag` is provided in the call to `setIamPolicy`, then the existing policy is
-   * overwritten.
+   * overwritten. Due to blind-set semantics of an etag-less policy, 'setIamPolicy' will not fail
+   * even if either of incoming or stored policy does not meet the version requirements.
    * @see #encodeEtag()
    * @param etag etag or {@code null} for none
    */
@@ -170,7 +191,8 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    * to the same version of the policy.
    *
    * If no `etag` is provided in the call to `setIamPolicy`, then the existing policy is
-   * overwritten.
+   * overwritten. Due to blind-set semantics of an etag-less policy, 'setIamPolicy' will not fail
+   * even if either of incoming or stored policy does not meet the version requirements.
    * @see #setEtag()
    *
    * <p>
@@ -189,8 +211,13 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    *
    * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
    *
-   * Policies with any conditional bindings must specify version 3. Policies without any conditional
-   * bindings may specify any valid value or leave the field unset.
+   * Operations affecting conditional bindings must specify version 3. This can be either setting a
+   * conditional policy, modifying a conditional binding, or removing a conditional binding from the
+   * stored conditional policy. Operations on non-conditional policies may specify any valid value
+   * or leave the field unset.
+   *
+   * If no etag is provided in the call to `setIamPolicy`, any version compliance checks on the
+   * incoming and/or stored policy is skipped.
    * @return value or {@code null} for none
    */
   public java.lang.Integer getVersion() {
@@ -202,8 +229,13 @@ public final class GoogleIamV1Policy extends com.google.api.client.json.GenericJ
    *
    * Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
    *
-   * Policies with any conditional bindings must specify version 3. Policies without any conditional
-   * bindings may specify any valid value or leave the field unset.
+   * Operations affecting conditional bindings must specify version 3. This can be either setting a
+   * conditional policy, modifying a conditional binding, or removing a conditional binding from the
+   * stored conditional policy. Operations on non-conditional policies may specify any valid value
+   * or leave the field unset.
+   *
+   * If no etag is provided in the call to `setIamPolicy`, any version compliance checks on the
+   * incoming and/or stored policy is skipped.
    * @param version version or {@code null} for none
    */
   public GoogleIamV1Policy setVersion(java.lang.Integer version) {

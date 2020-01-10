b75e3c4764ee67d6585dfe920e117496284ab1ec
yoshi-automation <yoshi-automation@google.com>
2020-01-10 04:04:43 -0800

    Regenerate gmail client

--- clients/google-api-services-gmail/v1/1.29.2/com/google/api/services/gmail/Gmail.java
+++ clients/google-api-services-gmail/v1/1.29.2/com/google/api/services/gmail/Gmail.java
@@ -5673,7 +5673,7 @@ public class Gmail extends com.google.api.client.googleapis.services.json.Abstra
          * verification email. The delegate user must be a member of the same G Suite organization as the
          * delegator user.
          *
-         * Gmail imposes limtations on the number of delegates and delegators each user in a G Suite
+         * Gmail imposes limitations on the number of delegates and delegators each user in a G Suite
          * organization can have. These limits depend on your organization, but in general each user can
          * have up to 25 delegates and up to 10 delegators.
          *
@@ -5711,7 +5711,7 @@ public class Gmail extends com.google.api.client.googleapis.services.json.Abstra
            * verification email. The delegate user must be a member of the same G Suite organization as the
            * delegator user.
            *
-           * Gmail imposes limtations on the number of delegates and delegators each user in a G Suite
+           * Gmail imposes limitations on the number of delegates and delegators each user in a G Suite
            * organization can have. These limits depend on your organization, but in general each user can
            * have up to 25 delegates and up to 10 delegators.
            *

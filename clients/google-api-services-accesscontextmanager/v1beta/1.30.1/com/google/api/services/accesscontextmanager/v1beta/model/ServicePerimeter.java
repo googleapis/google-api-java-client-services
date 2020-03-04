3b144aa1d61a797701bb0153885b1d235f9cd12e
yoshi-automation <yoshi-automation@google.com>
2020-03-04 03:37:05 -0800

    Regenerate accesscontextmanager client

--- clients/google-api-services-accesscontextmanager/v1beta/1.30.1/com/google/api/services/accesscontextmanager/v1beta/model/ServicePerimeter.java
+++ clients/google-api-services-accesscontextmanager/v1beta/1.30.1/com/google/api/services/accesscontextmanager/v1beta/model/ServicePerimeter.java
@@ -17,13 +17,14 @@
 package com.google.api.services.accesscontextmanager.v1beta.model;
 
 /**
- * `ServicePerimeter` describes a set of GCP resources which can freely import and export data
- * amongst themselves, but not export outside of the `ServicePerimeter`. If a request with a source
- * within this `ServicePerimeter` has a target outside of the `ServicePerimeter`, the request will
- * be blocked. Otherwise the request is allowed. There are two types of Service Perimeter - Regular
- * and Bridge. Regular Service Perimeters cannot overlap, a single GCP project can only belong to a
- * single regular Service Perimeter. Service Perimeter Bridges can contain only GCP projects as
- * members, a single GCP project may belong to multiple Service Perimeter Bridges.
+ * `ServicePerimeter` describes a set of Google Cloud resources which can freely import and export
+ * data amongst themselves, but not export outside of the `ServicePerimeter`. If a request with a
+ * source within this `ServicePerimeter` has a target outside of the `ServicePerimeter`, the request
+ * will be blocked. Otherwise the request is allowed. There are two types of Service Perimeter -
+ * Regular and Bridge. Regular Service Perimeters cannot overlap, a single Google Cloud project can
+ * only belong to a single regular Service Perimeter. Service Perimeter Bridges can contain only
+ * Google Cloud projects as members, a single Google Cloud project may belong to multiple Service
+ * Perimeter Bridges.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Access Context Manager API. For a detailed

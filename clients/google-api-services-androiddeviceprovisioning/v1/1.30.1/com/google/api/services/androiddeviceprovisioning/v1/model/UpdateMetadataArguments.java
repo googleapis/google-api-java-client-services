8a34bd5d52e80c4e315100d0a8d6d30da973cf44
yoshi-automation <yoshi-automation@google.com>
2020-03-18 03:40:12 -0700

    Regenerate androiddeviceprovisioning client

--- clients/google-api-services-androiddeviceprovisioning/v1/1.30.1/com/google/api/services/androiddeviceprovisioning/v1/model/UpdateMetadataArguments.java
+++ clients/google-api-services-androiddeviceprovisioning/v1/1.30.1/com/google/api/services/androiddeviceprovisioning/v1/model/UpdateMetadataArguments.java
@@ -17,7 +17,7 @@
 package com.google.api.services.androiddeviceprovisioning.v1.model;
 
 /**
- * Identifies metdata updates to one device.
+ * Identifies metadata updates to one device.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Android Device Provisioning Partner API. For a

75ccaa2d16b994c637165842699c56acb065ab3e
yoshi-automation <yoshi-automation@google.com>
2020-05-07 03:57:03 -0700

    Regenerate dataproc client

--- clients/google-api-services-dataproc/v1/1.30.1/com/google/api/services/dataproc/model/Policy.java
+++ clients/google-api-services-dataproc/v1/1.30.1/com/google/api/services/dataproc/model/Policy.java
@@ -21,23 +21,25 @@ package com.google.api.services.dataproc.model;
  * resources.A Policy is a collection of bindings. A binding binds one or more members to a single
  * role. Members can be user accounts, service accounts, Google groups, and domains (such as G
  * Suite). A role is a named list of permissions; each role can be an IAM predefined role or a user-
- * created custom role.Optionally, a binding can specify a condition, which is a logical expression
- * that allows access to a resource only if the expression evaluates to true. A condition can add
- * constraints based on attributes of the request, the resource, or both.JSON example: {
+ * created custom role.For some types of Google Cloud resources, a binding can also specify a
+ * condition, which is a logical expression that allows access to a resource only if the expression
+ * evaluates to true. A condition can add constraints based on attributes of the request, the
+ * resource, or both. To learn which resources support conditions in their IAM policies, see the IAM
+ * documentation (https://cloud.google.com/iam/help/conditions/resource-policies).JSON example: {
  * "bindings": [     {       "role": "roles/resourcemanager.organizationAdmin",       "members": [
  * "user:mike@example.com",         "group:admins@example.com",         "domain:google.com",
  * "serviceAccount:my-project-id@appspot.gserviceaccount.com"       ]     },     {       "role":
- * "roles/resourcemanager.organizationViewer",       "members": ["user:eve@example.com"],
- * "condition": {         "title": "expirable access",         "description": "Does not grant access
- * after Sep 2020",         "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')",
- * }     }   ],   "etag": "BwWWja0YfJA=",   "version": 3 } YAML example: bindings: - members:   -
- * user:mike@example.com   - group:admins@example.com   - domain:google.com   - serviceAccount:my-
- * project-id@appspot.gserviceaccount.com   role: roles/resourcemanager.organizationAdmin - members:
- * - user:eve@example.com   role: roles/resourcemanager.organizationViewer   condition:     title:
- * expirable access     description: Does not grant access after Sep 2020     expression:
- * request.time < timestamp('2020-10-01T00:00:00.000Z') - etag: BwWWja0YfJA= - version: 3 For a
- * description of IAM and its features, see the IAM documentation
- * (https://cloud.google.com/iam/docs/).
+ * "roles/resourcemanager.organizationViewer",       "members": [         "user:eve@example.com"
+ * ],       "condition": {         "title": "expirable access",         "description": "Does not
+ * grant access after Sep 2020",         "expression": "request.time <
+ * timestamp('2020-10-01T00:00:00.000Z')",       }     }   ],   "etag": "BwWWja0YfJA=",   "version":
+ * 3 } YAML example: bindings: - members:   - user:mike@example.com   - group:admins@example.com   -
+ * domain:google.com   - serviceAccount:my-project-id@appspot.gserviceaccount.com   role:
+ * roles/resourcemanager.organizationAdmin - members:   - user:eve@example.com   role:
+ * roles/resourcemanager.organizationViewer   condition:     title: expirable access
+ * description: Does not grant access after Sep 2020     expression: request.time <
+ * timestamp('2020-10-01T00:00:00.000Z') - etag: BwWWja0YfJA= - version: 3 For a description of IAM
+ * and its features, see the IAM documentation (https://cloud.google.com/iam/docs/).
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Cloud Dataproc API. For a detailed explanation see:
@@ -88,7 +90,9 @@ public final class Policy extends com.google.api.client.json.GenericJson {
    * whenever you call setIamPolicy. If you omit this field, then IAM allows you to overwrite a
    * version 3 policy with a version 1 policy, and all of the conditions in the version 3 policy are
    * lost.If a policy does not include any conditions, operations on that policy may specify any
-   * valid version or leave the field unset.
+   * valid version or leave the field unset.To learn which resources support conditions in their IAM
+   * policies, see the IAM documentation (https://cloud.google.com/iam/help/conditions/resource-
+   * policies).
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -200,7 +204,9 @@ public final class Policy extends com.google.api.client.json.GenericJson {
    * whenever you call setIamPolicy. If you omit this field, then IAM allows you to overwrite a
    * version 3 policy with a version 1 policy, and all of the conditions in the version 3 policy are
    * lost.If a policy does not include any conditions, operations on that policy may specify any
-   * valid version or leave the field unset.
+   * valid version or leave the field unset.To learn which resources support conditions in their IAM
+   * policies, see the IAM documentation (https://cloud.google.com/iam/help/conditions/resource-
+   * policies).
    * @return value or {@code null} for none
    */
   public java.lang.Integer getVersion() {
@@ -217,7 +223,9 @@ public final class Policy extends com.google.api.client.json.GenericJson {
    * whenever you call setIamPolicy. If you omit this field, then IAM allows you to overwrite a
    * version 3 policy with a version 1 policy, and all of the conditions in the version 3 policy are
    * lost.If a policy does not include any conditions, operations on that policy may specify any
-   * valid version or leave the field unset.
+   * valid version or leave the field unset.To learn which resources support conditions in their IAM
+   * policies, see the IAM documentation (https://cloud.google.com/iam/help/conditions/resource-
+   * policies).
    * @param version version or {@code null} for none
    */
   public Policy setVersion(java.lang.Integer version) {

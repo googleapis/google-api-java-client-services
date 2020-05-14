3f328e5729e6bda23df3d689856df4e15df4f1df
yoshi-automation <yoshi-automation@google.com>
2020-05-14 03:46:26 -0700

    Regenerate gamesManagement client

--- clients/google-api-services-gamesManagement/v1management/1.30.1/com/google/api/services/gamesManagement/model/GamesPlayerExperienceInfoResource.java
+++ clients/google-api-services-gamesManagement/v1management/1.30.1/com/google/api/services/gamesManagement/model/GamesPlayerExperienceInfoResource.java
@@ -17,11 +17,11 @@
 package com.google.api.services.gamesManagement.model;
 
 /**
- * This is a JSON template for 1P/3P metadata about the player's experience.
+ * 1P/3P metadata about the player's experience.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
- * transmitted over HTTP when working with the Google Play Game Services Management API. For a
- * detailed explanation see:
+ * transmitted over HTTP when working with the Google Play Game Management. For a detailed
+ * explanation see:
  * <a href="https://developers.google.com/api-client-library/java/google-http-java-client/json">https://developers.google.com/api-client-library/java/google-http-java-client/json</a>
  * </p>
  *

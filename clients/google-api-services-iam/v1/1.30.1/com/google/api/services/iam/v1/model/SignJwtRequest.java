826f0217253d04d94a4833952d92c0d204db24c1
yoshi-automation <yoshi-automation@google.com>
2020-06-12 03:47:20 -0700

    Regenerate iam client

--- clients/google-api-services-iam/v1/1.30.1/com/google/api/services/iam/v1/model/SignJwtRequest.java
+++ clients/google-api-services-iam/v1/1.30.1/com/google/api/services/iam/v1/model/SignJwtRequest.java
@@ -31,14 +31,28 @@ package com.google.api.services.iam.v1.model;
 public final class SignJwtRequest extends com.google.api.client.json.GenericJson {
 
   /**
-   * Required. The JWT payload to sign, a JSON JWT Claim set.
+   * Required. The JWT payload to sign. Must be a serialized JSON object that contains a JWT Claims
+   * Set. For example: `{"sub": "user@example.com", "iat": 313435}`
+   *
+   * If the JWT Claims Set contains an expiration time (`exp`) claim, it must be an integer
+   * timestamp that is not in the past and no more than 1 hour in the future.
+   *
+   * If the JWT Claims Set does not contain an expiration time (`exp`) claim, this claim is added
+   * automatically, with a timestamp that is 1 hour in the future.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
   private java.lang.String payload;
 
   /**
-   * Required. The JWT payload to sign, a JSON JWT Claim set.
+   * Required. The JWT payload to sign. Must be a serialized JSON object that contains a JWT Claims
+   * Set. For example: `{"sub": "user@example.com", "iat": 313435}`
+   *
+   * If the JWT Claims Set contains an expiration time (`exp`) claim, it must be an integer
+   * timestamp that is not in the past and no more than 1 hour in the future.
+   *
+   * If the JWT Claims Set does not contain an expiration time (`exp`) claim, this claim is added
+   * automatically, with a timestamp that is 1 hour in the future.
    * @return value or {@code null} for none
    */
   public java.lang.String getPayload() {
@@ -46,7 +60,14 @@ public final class SignJwtRequest extends com.google.api.client.json.GenericJson
   }
 
   /**
-   * Required. The JWT payload to sign, a JSON JWT Claim set.
+   * Required. The JWT payload to sign. Must be a serialized JSON object that contains a JWT Claims
+   * Set. For example: `{"sub": "user@example.com", "iat": 313435}`
+   *
+   * If the JWT Claims Set contains an expiration time (`exp`) claim, it must be an integer
+   * timestamp that is not in the past and no more than 1 hour in the future.
+   *
+   * If the JWT Claims Set does not contain an expiration time (`exp`) claim, this claim is added
+   * automatically, with a timestamp that is 1 hour in the future.
    * @param payload payload or {@code null} for none
    */
   public SignJwtRequest setPayload(java.lang.String payload) {

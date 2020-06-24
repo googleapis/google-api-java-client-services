3926902f08677f8f42c4851f166a9e8469004a06
yoshi-automation <yoshi-automation@google.com>
2020-06-24 03:51:48 -0700

    Regenerate monitoring client

--- clients/google-api-services-monitoring/v3/1.30.1/com/google/api/services/monitoring/v3/model/TimeSeriesDescriptor.java
+++ clients/google-api-services-monitoring/v3/1.30.1/com/google/api/services/monitoring/v3/model/TimeSeriesDescriptor.java
@@ -17,7 +17,7 @@
 package com.google.api.services.monitoring.v3.model;
 
 /**
- * A descriptor for the labels and points in a timeseries.
+ * A descriptor for the labels and points in a time series.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Cloud Monitoring API. For a detailed explanation see:

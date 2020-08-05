2f51168b34105bb1338533e668f5ed09a1b97c45
yoshi-automation <yoshi-automation@google.com>
2020-08-05 03:59:33 -0700

    Regenerate storagetransfer client

--- clients/google-api-services-storagetransfer/v1/1.30.1/com/google/api/services/storagetransfer/v1/model/TransferOptions.java
+++ clients/google-api-services-storagetransfer/v1/1.30.1/com/google/api/services/storagetransfer/v1/model/TransferOptions.java
@@ -17,8 +17,7 @@
 package com.google.api.services.storagetransfer.v1.model;
 
 /**
- * TransferOptions uses three boolean parameters to define the actions to be performed on objects in
- * a transfer.
+ * TransferOptions define the actions to be performed on objects in a transfer.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Storage Transfer API. For a detailed explanation see:

5fe320292c8ca3cae07c7294e0da491d54f0680d
yoshi-automation <yoshi-automation@google.com>
2020-08-06 03:56:15 -0700

    Regenerate servicenetworking client

--- clients/google-api-services-servicenetworking/v1/1.30.1/com/google/api/services/servicenetworking/v1/model/MonitoredResourceDescriptor.java
+++ clients/google-api-services-servicenetworking/v1/1.30.1/com/google/api/services/servicenetworking/v1/model/MonitoredResourceDescriptor.java
@@ -20,14 +20,9 @@ package com.google.api.services.servicenetworking.v1.model;
  * An object that describes the schema of a MonitoredResource object using a type name and a set of
  * labels. For example, the monitored resource descriptor for Google Compute Engine VM instances has
  * a type of `"gce_instance"` and specifies the use of the labels `"instance_id"` and `"zone"` to
- * identify particular VM instances. Different services can support different monitored resource
- * types. The following are specific rules to service defined monitored resources for Monitoring and
- * Logging: * The `type`, `display_name`, `description`, `labels` and `launch_stage` fields are all
- * required. * The first label of the monitored resource descriptor must be `resource_container`.
- * There are legacy monitored resource descritptors start with `project_id`. * It must include a
- * `location` label. * Maximum of default 5 service defined monitored resource descriptors is
- * allowed per service. * Maximum of default 10 labels per monitored resource is allowed. The
- * default maximum limit can be overridden. Please follow https://cloud.google.com/monitoring/quotas
+ * identify particular VM instances. Different APIs can support different monitored resource types.
+ * APIs generally provide a `list` method that returns the monitored resource descriptors used by
+ * the API.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Service Networking API. For a detailed explanation
@@ -58,11 +53,9 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   private java.lang.String displayName;
 
   /**
-   * Required. A set of labels used to describe instances of this monitored resource type. The label
-   * key name must follow: * Only upper and lower-case letters, digits and underscores (_) are
-   * allowed. * Label name must start with a letter or digit. * The maximum length of a label name
-   * is 100 characters. For example, an individual Google Cloud SQL database is identified by values
-   * for the labels `database_id` and `location`.
+   * Required. A set of labels used to describe instances of this monitored resource type. For
+   * example, an individual Google Cloud SQL database is identified by values for the labels
+   * `"database_id"` and `"zone"`.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -93,13 +86,8 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   private java.lang.String name;
 
   /**
-   * Required. The monitored resource type. For example, the type `cloudsql_database` represents
-   * databases in Google Cloud SQL. All service defined monitored resource types must be prefixed
-   * with the service name, in the format of `{service name}/{relative resource name}`. The relative
-   * resource name must follow: * Only upper and lower-case letters and digits are allowed. * It
-   * must start with upper case character and is recommended to use Upper Camel Case style. * The
-   * maximum number of characters allowed for the relative_resource_name is 100. Note there are
-   * legacy service monitored resources not following this rule.
+   * Required. The monitored resource type. For example, the type `"cloudsql_database"` represents
+   * databases in Google Cloud SQL.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -146,11 +134,9 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   }
 
   /**
-   * Required. A set of labels used to describe instances of this monitored resource type. The label
-   * key name must follow: * Only upper and lower-case letters, digits and underscores (_) are
-   * allowed. * Label name must start with a letter or digit. * The maximum length of a label name
-   * is 100 characters. For example, an individual Google Cloud SQL database is identified by values
-   * for the labels `database_id` and `location`.
+   * Required. A set of labels used to describe instances of this monitored resource type. For
+   * example, an individual Google Cloud SQL database is identified by values for the labels
+   * `"database_id"` and `"zone"`.
    * @return value or {@code null} for none
    */
   public java.util.List<LabelDescriptor> getLabels() {
@@ -158,11 +144,9 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   }
 
   /**
-   * Required. A set of labels used to describe instances of this monitored resource type. The label
-   * key name must follow: * Only upper and lower-case letters, digits and underscores (_) are
-   * allowed. * Label name must start with a letter or digit. * The maximum length of a label name
-   * is 100 characters. For example, an individual Google Cloud SQL database is identified by values
-   * for the labels `database_id` and `location`.
+   * Required. A set of labels used to describe instances of this monitored resource type. For
+   * example, an individual Google Cloud SQL database is identified by values for the labels
+   * `"database_id"` and `"zone"`.
    * @param labels labels or {@code null} for none
    */
   public MonitoredResourceDescriptor setLabels(java.util.List<LabelDescriptor> labels) {
@@ -213,13 +197,8 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   }
 
   /**
-   * Required. The monitored resource type. For example, the type `cloudsql_database` represents
-   * databases in Google Cloud SQL. All service defined monitored resource types must be prefixed
-   * with the service name, in the format of `{service name}/{relative resource name}`. The relative
-   * resource name must follow: * Only upper and lower-case letters and digits are allowed. * It
-   * must start with upper case character and is recommended to use Upper Camel Case style. * The
-   * maximum number of characters allowed for the relative_resource_name is 100. Note there are
-   * legacy service monitored resources not following this rule.
+   * Required. The monitored resource type. For example, the type `"cloudsql_database"` represents
+   * databases in Google Cloud SQL.
    * @return value or {@code null} for none
    */
   public java.lang.String getType() {
@@ -227,13 +206,8 @@ public final class MonitoredResourceDescriptor extends com.google.api.client.jso
   }
 
   /**
-   * Required. The monitored resource type. For example, the type `cloudsql_database` represents
-   * databases in Google Cloud SQL. All service defined monitored resource types must be prefixed
-   * with the service name, in the format of `{service name}/{relative resource name}`. The relative
-   * resource name must follow: * Only upper and lower-case letters and digits are allowed. * It
-   * must start with upper case character and is recommended to use Upper Camel Case style. * The
-   * maximum number of characters allowed for the relative_resource_name is 100. Note there are
-   * legacy service monitored resources not following this rule.
+   * Required. The monitored resource type. For example, the type `"cloudsql_database"` represents
+   * databases in Google Cloud SQL.
    * @param type type or {@code null} for none
    */
   public MonitoredResourceDescriptor setType(java.lang.String type) {

4640ddf8c95b6ed91abd428b495bdc34ab2158c2
yoshi-automation <yoshi-automation@google.com>
2020-08-21 03:54:04 -0700

    Regenerate monitoring client

--- clients/google-api-services-monitoring/v3/1.30.1/com/google/api/services/monitoring/v3/Monitoring.java
+++ clients/google-api-services-monitoring/v3/1.30.1/com/google/api/services/monitoring/v3/Monitoring.java
@@ -7486,26 +7486,28 @@ public class Monitoring extends com.google.api.client.googleapis.services.json.A
 
       /**
        * A filter specifying what Services to return. The filter currently supports the following
-       * fields: - `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` -
-       * `mesh_istio.mesh_uid` - `mesh_istio.service_namespace` - `mesh_istio.service_name` -
-       * `cluster_istio.location` (deprecated) - `cluster_istio.cluster_name` (deprecated) -
-       * `cluster_istio.service_namespace` (deprecated) - `cluster_istio.service_name` (deprecated)
-       * identifier_case refers to which option in the identifier oneof is populated. For example,
-       * the filter identifier_case = "CUSTOM" would match all services with a value for the custom
-       * field. Valid options are "CUSTOM", "APP_ENGINE", "CLOUD_ENDPOINTS", "MESH_ISTIO", and
-       * "CLUSTER_ISTIO" (deprecated),
+       * fields: - `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` (reserved
+       * for future use) - `mesh_istio.mesh_uid` - `mesh_istio.service_namespace` -
+       * `mesh_istio.service_name` - `cluster_istio.location` (deprecated) -
+       * `cluster_istio.cluster_name` (deprecated) - `cluster_istio.service_namespace` (deprecated)
+       * - `cluster_istio.service_name` (deprecated) identifier_case refers to which option in the
+       * identifier oneof is populated. For example, the filter identifier_case = "CUSTOM" would
+       * match all services with a value for the custom field. Valid options are "CUSTOM",
+       * "APP_ENGINE", "MESH_ISTIO", plus "CLUSTER_ISTIO" (deprecated) and "CLOUD_ENDPOINTS"
+       * (reserved for future use).
        */
       @com.google.api.client.util.Key
       private java.lang.String filter;
 
       /** A filter specifying what Services to return. The filter currently supports the following fields: -
-     `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` - `mesh_istio.mesh_uid` -
-     `mesh_istio.service_namespace` - `mesh_istio.service_name` - `cluster_istio.location` (deprecated)
-     - `cluster_istio.cluster_name` (deprecated) - `cluster_istio.service_namespace` (deprecated) -
-     `cluster_istio.service_name` (deprecated) identifier_case refers to which option in the identifier
-     oneof is populated. For example, the filter identifier_case = "CUSTOM" would match all services
-     with a value for the custom field. Valid options are "CUSTOM", "APP_ENGINE", "CLOUD_ENDPOINTS",
-     "MESH_ISTIO", and "CLUSTER_ISTIO" (deprecated),
+     `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` (reserved for future use) -
+     `mesh_istio.mesh_uid` - `mesh_istio.service_namespace` - `mesh_istio.service_name` -
+     `cluster_istio.location` (deprecated) - `cluster_istio.cluster_name` (deprecated) -
+     `cluster_istio.service_namespace` (deprecated) - `cluster_istio.service_name` (deprecated)
+     identifier_case refers to which option in the identifier oneof is populated. For example, the
+     filter identifier_case = "CUSTOM" would match all services with a value for the custom field. Valid
+     options are "CUSTOM", "APP_ENGINE", "MESH_ISTIO", plus "CLUSTER_ISTIO" (deprecated) and
+     "CLOUD_ENDPOINTS" (reserved for future use).
        */
       public java.lang.String getFilter() {
         return filter;
@@ -7513,14 +7515,15 @@ public class Monitoring extends com.google.api.client.googleapis.services.json.A
 
       /**
        * A filter specifying what Services to return. The filter currently supports the following
-       * fields: - `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` -
-       * `mesh_istio.mesh_uid` - `mesh_istio.service_namespace` - `mesh_istio.service_name` -
-       * `cluster_istio.location` (deprecated) - `cluster_istio.cluster_name` (deprecated) -
-       * `cluster_istio.service_namespace` (deprecated) - `cluster_istio.service_name` (deprecated)
-       * identifier_case refers to which option in the identifier oneof is populated. For example,
-       * the filter identifier_case = "CUSTOM" would match all services with a value for the custom
-       * field. Valid options are "CUSTOM", "APP_ENGINE", "CLOUD_ENDPOINTS", "MESH_ISTIO", and
-       * "CLUSTER_ISTIO" (deprecated),
+       * fields: - `identifier_case` - `app_engine.module_id` - `cloud_endpoints.service` (reserved
+       * for future use) - `mesh_istio.mesh_uid` - `mesh_istio.service_namespace` -
+       * `mesh_istio.service_name` - `cluster_istio.location` (deprecated) -
+       * `cluster_istio.cluster_name` (deprecated) - `cluster_istio.service_namespace` (deprecated)
+       * - `cluster_istio.service_name` (deprecated) identifier_case refers to which option in the
+       * identifier oneof is populated. For example, the filter identifier_case = "CUSTOM" would
+       * match all services with a value for the custom field. Valid options are "CUSTOM",
+       * "APP_ENGINE", "MESH_ISTIO", plus "CLUSTER_ISTIO" (deprecated) and "CLOUD_ENDPOINTS"
+       * (reserved for future use).
        */
       public List setFilter(java.lang.String filter) {
         this.filter = filter;

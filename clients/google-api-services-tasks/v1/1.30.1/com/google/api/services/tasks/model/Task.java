b0d1001c5eca184accea47a1765d5b91492e6864
yoshi-automation <yoshi-automation@google.com>
2020-09-16 03:55:02 -0700

    Regenerate tasks client

--- clients/google-api-services-tasks/v1/1.30.1/com/google/api/services/tasks/model/Task.java
+++ clients/google-api-services-tasks/v1/1.30.1/com/google/api/services/tasks/model/Task.java
@@ -17,7 +17,7 @@
 package com.google.api.services.tasks.model;
 
 /**
- * Model definition for Task.
+ * LINT.IfChange
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Tasks API. For a detailed explanation see:

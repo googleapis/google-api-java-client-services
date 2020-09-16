8b65330c6ba11a9fecd561dd38e9dbcf209501d6
yoshi-automation <yoshi-automation@google.com>
2020-09-16 03:49:15 -0700

    Regenerate licensing client

--- clients/google-api-services-licensing/v1/1.30.1/com/google/api/services/licensing/model/LicenseAssignmentInsert.java
+++ clients/google-api-services-licensing/v1/1.30.1/com/google/api/services/licensing/model/LicenseAssignmentInsert.java
@@ -20,7 +20,8 @@ package com.google.api.services.licensing.model;
  * Representation of a license assignment.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
- * transmitted over HTTP when working with the Licensing API. For a detailed explanation see:
+ * transmitted over HTTP when working with the Enterprise License Manager API. For a detailed
+ * explanation see:
  * <a href="https://developers.google.com/api-client-library/java/google-http-java-client/json">https://developers.google.com/api-client-library/java/google-http-java-client/json</a>
  * </p>
  *

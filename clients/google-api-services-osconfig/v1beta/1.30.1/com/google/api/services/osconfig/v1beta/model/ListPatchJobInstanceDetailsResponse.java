2cc35e85dcfc295800c1fa545c4cf6081c039a47
yoshi-automation <yoshi-automation@google.com>
2020-10-03 03:52:38 -0700

    Regenerate osconfig client

--- clients/google-api-services-osconfig/v1beta/1.30.1/com/google/api/services/osconfig/v1beta/model/ListPatchJobInstanceDetailsResponse.java
+++ clients/google-api-services-osconfig/v1beta/1.30.1/com/google/api/services/osconfig/v1beta/model/ListPatchJobInstanceDetailsResponse.java
@@ -20,7 +20,7 @@ package com.google.api.services.osconfig.v1beta.model;
  * A response message for listing the instances details for a patch job.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
- * transmitted over HTTP when working with the Cloud OS Config API. For a detailed explanation see:
+ * transmitted over HTTP when working with the OS Config API. For a detailed explanation see:
  * <a href="https://developers.google.com/api-client-library/java/google-http-java-client/json">https://developers.google.com/api-client-library/java/google-http-java-client/json</a>
  * </p>
  *

c893a65dea202c6d8dd5ab57b2058e1f2c63f956
yoshi-automation <yoshi-automation@google.com>
2020-10-21 03:48:57 -0700

    Regenerate dns client

--- clients/google-api-services-dns/v1beta2/1.30.1/com/google/api/services/dns/Dns.java
+++ clients/google-api-services-dns/v1beta2/1.30.1/com/google/api/services/dns/Dns.java
@@ -25,7 +25,7 @@ package com.google.api.services.dns;
  *
  * <p>
  * For more information about this service, see the
- * <a href="http://developers.google.com/cloud-dns" target="_blank">API Documentation</a>
+ * <a href="https://cloud.google.com/dns/docs" target="_blank">API Documentation</a>
  * </p>
  *
  * <p>

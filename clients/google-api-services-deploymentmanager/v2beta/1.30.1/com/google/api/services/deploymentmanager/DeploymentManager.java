a7093a936daca9b04740ee7f1b05bbddcf936d11
yoshi-automation <yoshi-automation@google.com>
2020-11-13 03:44:15 -0800

    Regenerate deploymentmanager client

--- clients/google-api-services-deploymentmanager/v2beta/1.30.1/com/google/api/services/deploymentmanager/DeploymentManager.java
+++ clients/google-api-services-deploymentmanager/v2beta/1.30.1/com/google/api/services/deploymentmanager/DeploymentManager.java
@@ -25,7 +25,7 @@ package com.google.api.services.deploymentmanager;
  *
  * <p>
  * For more information about this service, see the
- * <a href="http://cloud.google.com/deployment-manager" target="_blank">API Documentation</a>
+ * <a href="https://cloud.google.com/deployment-manager" target="_blank">API Documentation</a>
  * </p>
  *
  * <p>

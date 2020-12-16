cab954097992083734730606a4c8310beedbc07b
yoshi-automation <yoshi-automation@google.com>
2020-12-16 04:04:59 -0800

    Regenerate storagetransfer client

--- clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/GcsData.java
+++ clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/GcsData.java
@@ -40,6 +40,16 @@ public final class GcsData extends com.google.api.client.json.GenericJson {
   private java.lang.String bucketName;
 
   /**
+   * Root path to transfer objects. Must be an empty string or full path name that ends with a '/'.
+   * This field is treated as an object prefix. As such, it should generally not begin with a '/'.
+   * (must meet Object Name
+   * Requirements](https://cloud.google.com/storage/docs/naming#objectnames)).
+   * The value may be {@code null}.
+   */
+  @com.google.api.client.util.Key
+  private java.lang.String path;
+
+  /**
    * Required. Cloud Storage bucket name (see [Bucket Name
    * Requirements](https://cloud.google.com/storage/docs/naming#requirements)).
    * @return value or {@code null} for none
@@ -55,6 +65,29 @@ public final class GcsData extends com.google.api.client.json.GenericJson {
    */
   public GcsData setBucketName(java.lang.String bucketName) {
     this.bucketName = bucketName;
+    return this;
+  }
+
+  /**
+   * Root path to transfer objects. Must be an empty string or full path name that ends with a '/'.
+   * This field is treated as an object prefix. As such, it should generally not begin with a '/'.
+   * (must meet Object Name
+   * Requirements](https://cloud.google.com/storage/docs/naming#objectnames)).
+   * @return value or {@code null} for none
+   */
+  public java.lang.String getPath() {
+    return path;
+  }
+
+  /**
+   * Root path to transfer objects. Must be an empty string or full path name that ends with a '/'.
+   * This field is treated as an object prefix. As such, it should generally not begin with a '/'.
+   * (must meet Object Name
+   * Requirements](https://cloud.google.com/storage/docs/naming#objectnames)).
+   * @param path path or {@code null} for none
+   */
+  public GcsData setPath(java.lang.String path) {
+    this.path = path;
     return this;
   }
 

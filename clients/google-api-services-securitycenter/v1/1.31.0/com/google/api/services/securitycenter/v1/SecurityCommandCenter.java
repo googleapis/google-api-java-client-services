86573438728442dec47b10540436b2013b0c58b9
yoshi-automation <yoshi-automation@google.com>
2021-01-07 03:58:14 -0800

    Regenerate securitycenter client

--- clients/google-api-services-securitycenter/v1/1.31.0/com/google/api/services/securitycenter/v1/SecurityCommandCenter.java
+++ clients/google-api-services-securitycenter/v1/1.31.0/com/google/api/services/securitycenter/v1/SecurityCommandCenter.java
@@ -25,7 +25,7 @@ package com.google.api.services.securitycenter.v1;
  *
  * <p>
  * For more information about this service, see the
- * <a href="https://console.cloud.google.com/apis/api/securitycenter.googleapis.com/overview" target="_blank">API Documentation</a>
+ * <a href="https://cloud.google.com/security-command-center" target="_blank">API Documentation</a>
  * </p>
  *
  * <p>

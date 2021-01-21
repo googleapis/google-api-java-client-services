339bfc69216e79893580a3bb0c509f7f1b3b3cf9
yoshi-automation <yoshi-automation@google.com>
2021-01-21 03:41:32 -0800

    Regenerate analytics client

--- clients/google-api-services-analyticsadmin/v1alpha/1.31.0/com/google/api/services/analyticsadmin/v1alpha/GoogleAnalyticsAdmin.java
+++ clients/google-api-services-analyticsadmin/v1alpha/1.31.0/com/google/api/services/analyticsadmin/v1alpha/GoogleAnalyticsAdmin.java
@@ -3561,7 +3561,11 @@ public class GoogleAnalyticsAdmin extends com.google.api.client.googleapis.servi
     public class AndroidAppDataStreams {
 
       /**
-       * Creates an android app stream with the specified location and attributes.
+       * Creates an Android app stream with the specified location and attributes. Note that an Android
+       * app stream must be linked to a Firebase app to receive traffic. To create a working app stream,
+       * make sure your property is linked to a Firebase project. Then, use the Firebase API to create a
+       * Firebase app, which will also create an appropriate data stream in Analytics (may take up to 24
+       * hours).
        *
        * Create a request for the method "androidAppDataStreams.create".
        *
@@ -3587,7 +3591,11 @@ public class GoogleAnalyticsAdmin extends com.google.api.client.googleapis.servi
             java.util.regex.Pattern.compile("^properties/[^/]+$");
 
         /**
-         * Creates an android app stream with the specified location and attributes.
+         * Creates an Android app stream with the specified location and attributes. Note that an Android
+         * app stream must be linked to a Firebase app to receive traffic. To create a working app stream,
+         * make sure your property is linked to a Firebase project. Then, use the Firebase API to create a
+         * Firebase app, which will also create an appropriate data stream in Analytics (may take up to 24
+         * hours).
          *
          * Create a request for the method "androidAppDataStreams.create".
          *
@@ -5653,7 +5661,10 @@ public class GoogleAnalyticsAdmin extends com.google.api.client.googleapis.servi
     public class IosAppDataStreams {
 
       /**
-       * Creates an iOS app data stream with the specified location and attributes.
+       * Creates an iOS app stream with the specified location and attributes. Note that an iOS app stream
+       * must be linked to a Firebase app to receive traffic. To create a working app stream, make sure
+       * your property is linked to a Firebase project. Then, use the Firebase API to create a Firebase
+       * app, which will also create an appropriate data stream in Analytics (may take up to 24 hours).
        *
        * Create a request for the method "iosAppDataStreams.create".
        *
@@ -5678,7 +5689,11 @@ public class GoogleAnalyticsAdmin extends com.google.api.client.googleapis.servi
             java.util.regex.Pattern.compile("^properties/[^/]+$");
 
         /**
-         * Creates an iOS app data stream with the specified location and attributes.
+         * Creates an iOS app stream with the specified location and attributes. Note that an iOS app
+         * stream must be linked to a Firebase app to receive traffic. To create a working app stream,
+         * make sure your property is linked to a Firebase project. Then, use the Firebase API to create a
+         * Firebase app, which will also create an appropriate data stream in Analytics (may take up to 24
+         * hours).
          *
          * Create a request for the method "iosAppDataStreams.create".
          *

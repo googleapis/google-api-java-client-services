03c26b7fc1cdede75731095f291af49144193b36
yoshi-automation <yoshi-automation@google.com>
2021-01-26 03:47:32 -0800

    Regenerate file client

--- clients/google-api-services-file/v1/1.31.0/com/google/api/services/file/v1/CloudFilestore.java
+++ clients/google-api-services-file/v1/1.31.0/com/google/api/services/file/v1/CloudFilestore.java
@@ -2245,8 +2245,9 @@ public class CloudFilestore extends com.google.api.client.googleapis.services.js
           }
         }
         /**
-         * Restores an existing instance's file share from a backup. The instance's file share capacity will
-         * be set to the backup's capacity or the minimum capacity of the tier, whichever is larger.
+         * Restores an existing instance's file share from a backup. The capacity of the instance needs to
+         * be equal to or larger than the capacity of the backup (and also equal to or larger than the
+         * minimum capacity of the tier).
          *
          * Create a request for the method "instances.restore".
          *
@@ -2272,8 +2273,9 @@ public class CloudFilestore extends com.google.api.client.googleapis.services.js
               java.util.regex.Pattern.compile("^projects/[^/]+/locations/[^/]+/instances/[^/]+$");
 
           /**
-           * Restores an existing instance's file share from a backup. The instance's file share capacity
-           * will be set to the backup's capacity or the minimum capacity of the tier, whichever is larger.
+           * Restores an existing instance's file share from a backup. The capacity of the instance needs to
+           * be equal to or larger than the capacity of the backup (and also equal to or larger than the
+           * minimum capacity of the tier).
            *
            * Create a request for the method "instances.restore".
            *

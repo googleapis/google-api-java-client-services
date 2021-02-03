f6174d606601ce1e1b72676bff5be35aa2516e03
yoshi-automation <yoshi-automation@google.com>
2021-02-03 04:02:04 -0800

    Regenerate storagetransfer client

--- clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/AwsS3Data.java
+++ clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/AwsS3Data.java
@@ -33,8 +33,8 @@ public final class AwsS3Data extends com.google.api.client.json.GenericJson {
   /**
    * Required. Input only. AWS access key used to sign the API requests to the AWS S3 bucket.
    * Permissions on the bucket must be granted to the access ID of the AWS access key. For
-   * information on our data retention policy for user credentials, see [User credentials](data-
-   * retention#user-credentials).
+   * information on our data retention policy for user credentials, see [User credentials](/storage-
+   * transfer/docs/data-retention#user-credentials).
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -60,8 +60,8 @@ public final class AwsS3Data extends com.google.api.client.json.GenericJson {
   /**
    * Required. Input only. AWS access key used to sign the API requests to the AWS S3 bucket.
    * Permissions on the bucket must be granted to the access ID of the AWS access key. For
-   * information on our data retention policy for user credentials, see [User credentials](data-
-   * retention#user-credentials).
+   * information on our data retention policy for user credentials, see [User credentials](/storage-
+   * transfer/docs/data-retention#user-credentials).
    * @return value or {@code null} for none
    */
   public AwsAccessKey getAwsAccessKey() {
@@ -71,8 +71,8 @@ public final class AwsS3Data extends com.google.api.client.json.GenericJson {
   /**
    * Required. Input only. AWS access key used to sign the API requests to the AWS S3 bucket.
    * Permissions on the bucket must be granted to the access ID of the AWS access key. For
-   * information on our data retention policy for user credentials, see [User credentials](data-
-   * retention#user-credentials).
+   * information on our data retention policy for user credentials, see [User credentials](/storage-
+   * transfer/docs/data-retention#user-credentials).
    * @param awsAccessKey awsAccessKey or {@code null} for none
    */
   public AwsS3Data setAwsAccessKey(AwsAccessKey awsAccessKey) {

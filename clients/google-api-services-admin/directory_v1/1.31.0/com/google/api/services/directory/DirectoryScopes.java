92ba6a7cf50c6229b89c1f41cd2099020a0bbd4a
yoshi-automation <yoshi-automation@google.com>
2021-02-24 03:41:19 -0800

    Regenerate admin client

--- clients/google-api-services-admin/directory_v1/1.31.0/com/google/api/services/directory/DirectoryScopes.java
+++ clients/google-api-services-admin/directory_v1/1.31.0/com/google/api/services/directory/DirectoryScopes.java
@@ -89,7 +89,7 @@ public class DirectoryScopes {
   /** View user aliases on your domain. */
   public static final String ADMIN_DIRECTORY_USER_ALIAS_READONLY = "https://www.googleapis.com/auth/admin.directory.user.alias.readonly";
 
-  /** View users on your domain. */
+  /** See info about users on your domain. */
   public static final String ADMIN_DIRECTORY_USER_READONLY = "https://www.googleapis.com/auth/admin.directory.user.readonly";
 
   /** Manage data access permissions for users on your domain. */

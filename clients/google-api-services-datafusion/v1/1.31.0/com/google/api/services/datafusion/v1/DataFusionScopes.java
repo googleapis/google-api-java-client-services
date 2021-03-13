6e113133111770653bd307434b2081d4d3dd168e
yoshi-automation <yoshi-automation@google.com>
2021-03-13 03:45:03 -0800

    Regenerate datafusion client

--- clients/google-api-services-datafusion/v1/1.31.0/com/google/api/services/datafusion/v1/DataFusionScopes.java
+++ clients/google-api-services-datafusion/v1/1.31.0/com/google/api/services/datafusion/v1/DataFusionScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.datafusion.v1;
  */
 public class DataFusionScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /**

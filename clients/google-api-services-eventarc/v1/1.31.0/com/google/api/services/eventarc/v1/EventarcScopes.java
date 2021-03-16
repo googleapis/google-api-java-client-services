8672afda3804b3f84e248fd82e8f0d2fc5af8ff4
yoshi-automation <yoshi-automation@google.com>
2021-03-16 03:51:25 -0700

    Regenerate eventarc client

--- clients/google-api-services-eventarc/v1/1.31.0/com/google/api/services/eventarc/v1/EventarcScopes.java
+++ clients/google-api-services-eventarc/v1/1.31.0/com/google/api/services/eventarc/v1/EventarcScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.eventarc.v1;
  */
 public class EventarcScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /**

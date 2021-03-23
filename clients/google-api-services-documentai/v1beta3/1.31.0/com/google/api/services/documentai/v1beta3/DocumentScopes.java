e0cd472aaea00f87962cea3f52807c44e8751252
yoshi-automation <yoshi-automation@google.com>
2021-03-23 03:50:39 -0700

    Regenerate documentai client

--- clients/google-api-services-documentai/v1beta3/1.31.0/com/google/api/services/documentai/v1beta3/DocumentScopes.java
+++ clients/google-api-services-documentai/v1beta3/1.31.0/com/google/api/services/documentai/v1beta3/DocumentScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.documentai.v1beta3;
  */
 public class DocumentScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /**

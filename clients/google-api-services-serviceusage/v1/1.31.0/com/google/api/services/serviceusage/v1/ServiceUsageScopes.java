37c9090e3f17fb29e72dec140a3b72ce61e43fb6
yoshi-automation <yoshi-automation@google.com>
2021-03-23 04:02:17 -0700

    Regenerate serviceusage client

--- clients/google-api-services-serviceusage/v1/1.31.0/com/google/api/services/serviceusage/v1/ServiceUsageScopes.java
+++ clients/google-api-services-serviceusage/v1/1.31.0/com/google/api/services/serviceusage/v1/ServiceUsageScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.serviceusage.v1;
  */
 public class ServiceUsageScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** View your data across Google Cloud Platform services. */

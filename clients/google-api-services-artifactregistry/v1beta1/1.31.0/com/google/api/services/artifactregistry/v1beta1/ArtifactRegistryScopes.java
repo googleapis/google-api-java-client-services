5c8446ff3aba0b97bd0825b1e84913ce89a9d72d
yoshi-automation <yoshi-automation@google.com>
2021-03-30 03:43:37 -0700

    Regenerate artifactregistry client

--- clients/google-api-services-artifactregistry/v1beta1/1.31.0/com/google/api/services/artifactregistry/v1beta1/ArtifactRegistryScopes.java
+++ clients/google-api-services-artifactregistry/v1beta1/1.31.0/com/google/api/services/artifactregistry/v1beta1/ArtifactRegistryScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.artifactregistry.v1beta1;
  */
 public class ArtifactRegistryScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** View your data across Google Cloud Platform services. */

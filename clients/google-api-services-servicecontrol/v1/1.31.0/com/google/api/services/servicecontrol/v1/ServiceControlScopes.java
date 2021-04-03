0b7ec112a4af53a71786829cd6453ef590d3904b
yoshi-automation <yoshi-automation@google.com>
2021-04-03 03:57:37 -0700

    Regenerate servicecontrol client

--- clients/google-api-services-servicecontrol/v1/1.31.0/com/google/api/services/servicecontrol/v1/ServiceControlScopes.java
+++ clients/google-api-services-servicecontrol/v1/1.31.0/com/google/api/services/servicecontrol/v1/ServiceControlScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.servicecontrol.v1;
  */
 public class ServiceControlScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** Manage your Google Service Control data. */

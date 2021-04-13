8c710434e7951762a83ed2928cfd5112a55a31d9
yoshi-automation <yoshi-automation@google.com>
2021-04-13 03:49:32 -0700

    Regenerate firestore client

--- clients/google-api-services-firestore/v1/1.31.0/com/google/api/services/firestore/v1/FirestoreScopes.java
+++ clients/google-api-services-firestore/v1/1.31.0/com/google/api/services/firestore/v1/FirestoreScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.firestore.v1;
  */
 public class FirestoreScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** View and manage your Google Cloud Datastore data. */

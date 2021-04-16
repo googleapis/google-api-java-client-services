e97ff062f159ed3294f9ccfe5d192da72a9204dd
yoshi-automation <yoshi-automation@google.com>
2021-04-16 03:58:46 -0700

    Regenerate memcache client

--- clients/google-api-services-memcache/v1/1.31.0/com/google/api/services/memcache/v1/CloudMemorystoreforMemcachedScopes.java
+++ clients/google-api-services-memcache/v1/1.31.0/com/google/api/services/memcache/v1/CloudMemorystoreforMemcachedScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.memcache.v1;
  */
 public class CloudMemorystoreforMemcachedScopes {
 
-  /** View and manage your data across Google Cloud Platform services. */
+  /** See, edit, configure, and delete your Google Cloud Platform data. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /**

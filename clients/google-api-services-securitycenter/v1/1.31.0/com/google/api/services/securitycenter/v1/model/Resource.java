242f71dcf654a1891dfd21154ba51cb4313cabfd
yoshi-automation <yoshi-automation@google.com>
2021-04-27 04:03:15 -0700

    Regenerate securitycenter client

--- clients/google-api-services-securitycenter/v1/1.31.0/com/google/api/services/securitycenter/v1/model/Resource.java
+++ clients/google-api-services-securitycenter/v1/1.31.0/com/google/api/services/securitycenter/v1/model/Resource.java
@@ -18,7 +18,6 @@ package com.google.api.services.securitycenter.v1.model;
 
 /**
  * Information related to the Google Cloud resource that is associated with this finding.
- * LINT.IfChange
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Security Command Center API. For a detailed

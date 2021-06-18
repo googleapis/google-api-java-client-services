d543b089bc8120dc092a01542690078a4a877e4e
yoshi-automation <yoshi-automation@google.com>
2021-06-18 03:57:02 -0700

    Regenerate driveactivity client

--- clients/google-api-services-driveactivity/v2/1.31.0/com/google/api/services/driveactivity/v2/DriveActivity.java
+++ clients/google-api-services-driveactivity/v2/1.31.0/com/google/api/services/driveactivity/v2/DriveActivity.java
@@ -48,7 +48,7 @@ public class DriveActivity extends com.google.api.client.googleapis.services.jso
         com.google.api.client.googleapis.GoogleUtils.BUGFIX_VERSION >= 1)),
         "You are currently running with version %s of google-api-client. " +
         "You need at least version 1.31.1 of google-api-client to run version " +
-        "1.31.0 of the Drive Activity API library.", com.google.api.client.googleapis.GoogleUtils.VERSION);
+        "1.31.5 of the Drive Activity API library.", com.google.api.client.googleapis.GoogleUtils.VERSION);
   }
 
   /**

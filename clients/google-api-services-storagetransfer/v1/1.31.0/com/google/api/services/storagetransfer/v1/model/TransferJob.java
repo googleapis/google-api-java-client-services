a4c918b300a2979f425455c2d81c254a33e0e226
yoshi-automation <yoshi-automation@google.com>
2021-07-14 04:04:58 -0700

    Regenerate storagetransfer client

--- clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/TransferJob.java
+++ clients/google-api-services-storagetransfer/v1/1.31.0/com/google/api/services/storagetransfer/v1/model/TransferJob.java
@@ -72,16 +72,20 @@ public final class TransferJob extends com.google.api.client.json.GenericJson {
    * the specified name is used as the unique name for this job. If the specified name is in use by
    * a job, the creation request fails with an ALREADY_EXISTS error. This name must start with
    * `"transferJobs/"` prefix and end with a letter or a number, and should be no more than 128
-   * characters. This name must not start with 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved
-   * prefix. Example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Invalid job names fail
-   * with an INVALID_ARGUMENT error.
+   * characters. For transfers involving PosixFilesystem, this name must start with
+   * 'transferJobs/OPI' specifically. For all other transfer types, this name must not start with
+   * 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved prefix for PosixFilesystem transfers. Non-
+   * PosixFilesystem example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` PosixFilesystem
+   * example: `"transferJobs/OPI^[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Applications must not rely on the
+   * enforcement of naming requirements involving OPI. Invalid job names fail with an
+   * INVALID_ARGUMENT error.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
   private java.lang.String name;
 
   /**
-   * Notification configuration.
+   * Notification configuration. This is not supported for transfers involving PosixFilesystem.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -215,9 +219,13 @@ public final class TransferJob extends com.google.api.client.json.GenericJson {
    * the specified name is used as the unique name for this job. If the specified name is in use by
    * a job, the creation request fails with an ALREADY_EXISTS error. This name must start with
    * `"transferJobs/"` prefix and end with a letter or a number, and should be no more than 128
-   * characters. This name must not start with 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved
-   * prefix. Example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Invalid job names fail
-   * with an INVALID_ARGUMENT error.
+   * characters. For transfers involving PosixFilesystem, this name must start with
+   * 'transferJobs/OPI' specifically. For all other transfer types, this name must not start with
+   * 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved prefix for PosixFilesystem transfers. Non-
+   * PosixFilesystem example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` PosixFilesystem
+   * example: `"transferJobs/OPI^[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Applications must not rely on the
+   * enforcement of naming requirements involving OPI. Invalid job names fail with an
+   * INVALID_ARGUMENT error.
    * @return value or {@code null} for none
    */
   public java.lang.String getName() {
@@ -230,9 +238,13 @@ public final class TransferJob extends com.google.api.client.json.GenericJson {
    * the specified name is used as the unique name for this job. If the specified name is in use by
    * a job, the creation request fails with an ALREADY_EXISTS error. This name must start with
    * `"transferJobs/"` prefix and end with a letter or a number, and should be no more than 128
-   * characters. This name must not start with 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved
-   * prefix. Example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Invalid job names fail
-   * with an INVALID_ARGUMENT error.
+   * characters. For transfers involving PosixFilesystem, this name must start with
+   * 'transferJobs/OPI' specifically. For all other transfer types, this name must not start with
+   * 'transferJobs/OPI'. 'transferJobs/OPI' is a reserved prefix for PosixFilesystem transfers. Non-
+   * PosixFilesystem example: `"transferJobs/^(?!OPI)[A-Za-z0-9-._~]*[A-Za-z0-9]$"` PosixFilesystem
+   * example: `"transferJobs/OPI^[A-Za-z0-9-._~]*[A-Za-z0-9]$"` Applications must not rely on the
+   * enforcement of naming requirements involving OPI. Invalid job names fail with an
+   * INVALID_ARGUMENT error.
    * @param name name or {@code null} for none
    */
   public TransferJob setName(java.lang.String name) {
@@ -241,7 +253,7 @@ public final class TransferJob extends com.google.api.client.json.GenericJson {
   }
 
   /**
-   * Notification configuration.
+   * Notification configuration. This is not supported for transfers involving PosixFilesystem.
    * @return value or {@code null} for none
    */
   public NotificationConfig getNotificationConfig() {
@@ -249,7 +261,7 @@ public final class TransferJob extends com.google.api.client.json.GenericJson {
   }
 
   /**
-   * Notification configuration.
+   * Notification configuration. This is not supported for transfers involving PosixFilesystem.
    * @param notificationConfig notificationConfig or {@code null} for none
    */
   public TransferJob setNotificationConfig(NotificationConfig notificationConfig) {

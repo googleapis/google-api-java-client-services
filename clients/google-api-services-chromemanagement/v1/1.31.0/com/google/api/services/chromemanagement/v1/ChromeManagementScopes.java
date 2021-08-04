dea3a614066966448c0d383cebde84d8c8f90050
yoshi-automation <yoshi-automation@google.com>
2021-08-04 03:44:06 -0700

    Regenerate chromemanagement client

--- clients/google-api-services-chromemanagement/v1/1.31.0/com/google/api/services/chromemanagement/v1/ChromeManagementScopes.java
+++ clients/google-api-services-chromemanagement/v1/1.31.0/com/google/api/services/chromemanagement/v1/ChromeManagementScopes.java
@@ -23,6 +23,9 @@ package com.google.api.services.chromemanagement.v1;
  */
 public class ChromeManagementScopes {
 
+  /** See detailed information about apps installed on Chrome browsers and devices managed by your organization. */
+  public static final String CHROME_MANAGEMENT_APPDETAILS_READONLY = "https://www.googleapis.com/auth/chrome.management.appdetails.readonly";
+
   /** See reports about devices and Chrome browsers managed within your organization. */
   public static final String CHROME_MANAGEMENT_REPORTS_READONLY = "https://www.googleapis.com/auth/chrome.management.reports.readonly";
 
@@ -33,6 +36,7 @@ public class ChromeManagementScopes {
    */
   public static java.util.Set<String> all() {
     java.util.Set<String> set = new java.util.HashSet<String>();
+    set.add(CHROME_MANAGEMENT_APPDETAILS_READONLY);
     set.add(CHROME_MANAGEMENT_REPORTS_READONLY);
     return java.util.Collections.unmodifiableSet(set);
   }

4358588a95343c6f5d0c8dfcb2d43545ac3b88a3
yoshi-automation <yoshi-automation@google.com>
2021-08-11 04:00:12 -0700

    Regenerate run client

--- clients/google-api-services-runtimeconfig/v1/1.31.0/com/google/api/services/runtimeconfig/v1/CloudRuntimeConfigScopes.java
+++ clients/google-api-services-runtimeconfig/v1/1.31.0/com/google/api/services/runtimeconfig/v1/CloudRuntimeConfigScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.runtimeconfig.v1;
  */
 public class CloudRuntimeConfigScopes {
 
-  /** See, edit, configure, and delete your Google Cloud Platform data. */
+  /** See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** Manage your Google Cloud Platform services' runtime configuration. */

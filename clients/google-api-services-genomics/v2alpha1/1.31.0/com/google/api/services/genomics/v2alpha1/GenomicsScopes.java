ca2badeb9924a4116ee67d4a995b78e920b3ae22
yoshi-automation <yoshi-automation@google.com>
2021-08-13 03:53:06 -0700

    Regenerate genomics client

--- clients/google-api-services-genomics/v2alpha1/1.31.0/com/google/api/services/genomics/v2alpha1/GenomicsScopes.java
+++ clients/google-api-services-genomics/v2alpha1/1.31.0/com/google/api/services/genomics/v2alpha1/GenomicsScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.genomics.v2alpha1;
  */
 public class GenomicsScopes {
 
-  /** See, edit, configure, and delete your Google Cloud Platform data. */
+  /** See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** View and manage Genomics data. */

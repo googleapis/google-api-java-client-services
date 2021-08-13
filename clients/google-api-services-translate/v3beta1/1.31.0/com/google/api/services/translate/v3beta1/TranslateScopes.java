ff078de29fa3b99222bb51a7062bd3e7fbb71199
yoshi-automation <yoshi-automation@google.com>
2021-08-13 04:04:34 -0700

    Regenerate translate client

--- clients/google-api-services-translate/v3beta1/1.31.0/com/google/api/services/translate/v3beta1/TranslateScopes.java
+++ clients/google-api-services-translate/v3beta1/1.31.0/com/google/api/services/translate/v3beta1/TranslateScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.translate.v3beta1;
  */
 public class TranslateScopes {
 
-  /** See, edit, configure, and delete your Google Cloud Platform data. */
+  /** See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** Translate text from one language to another using Google Translate. */

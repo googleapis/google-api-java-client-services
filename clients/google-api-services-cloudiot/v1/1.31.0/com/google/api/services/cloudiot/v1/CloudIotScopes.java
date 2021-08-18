d4b4eee727de6d7143cd7f1308bbd3d3108881e9
yoshi-automation <yoshi-automation@google.com>
2021-08-18 03:45:19 -0700

    Regenerate cloudiot client

--- clients/google-api-services-cloudiot/v1/1.31.0/com/google/api/services/cloudiot/v1/CloudIotScopes.java
+++ clients/google-api-services-cloudiot/v1/1.31.0/com/google/api/services/cloudiot/v1/CloudIotScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.cloudiot.v1;
  */
 public class CloudIotScopes {
 
-  /** See, edit, configure, and delete your Google Cloud Platform data. */
+  /** See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** Register and manage devices in the Google Cloud IoT service. */

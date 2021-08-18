8b353c36c10c2bbc22c20dd367c91e9356011722
yoshi-automation <yoshi-automation@google.com>
2021-08-18 03:46:29 -0700

    Regenerate composer client

--- clients/google-api-services-composer/v1beta1/1.31.0/com/google/api/services/composer/v1beta1/model/IPAllocationPolicy.java
+++ clients/google-api-services-composer/v1beta1/1.31.0/com/google/api/services/composer/v1beta1/model/IPAllocationPolicy.java
@@ -34,7 +34,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both.
@@ -59,7 +59,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `services_secondary_range_name` or `services_ipv4_cidr_block` but not both.
@@ -94,7 +94,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both.
@@ -109,7 +109,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both.
@@ -150,7 +150,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `services_secondary_range_name` or `services_ipv4_cidr_block` but not both.
@@ -165,7 +165,7 @@ public final class IPAllocationPolicy extends com.google.api.client.json.Generic
    * applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions
    * composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set
    * to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have
-   * GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki
+   * GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki
    * /Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private
    * networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to
    * use. Specify `services_secondary_range_name` or `services_ipv4_cidr_block` but not both.

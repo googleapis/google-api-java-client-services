fca3d7bf6776a34e32b11746db9fbf1fd256bc5f
yoshi-automation <yoshi-automation@google.com>
2021-08-19 03:45:11 -0700

    Regenerate cloudtrace client

--- clients/google-api-services-cloudtrace/v2beta1/1.31.0/com/google/api/services/cloudtrace/v2beta1/CloudTraceScopes.java
+++ clients/google-api-services-cloudtrace/v2beta1/1.31.0/com/google/api/services/cloudtrace/v2beta1/CloudTraceScopes.java
@@ -23,7 +23,7 @@ package com.google.api.services.cloudtrace.v2beta1;
  */
 public class CloudTraceScopes {
 
-  /** See, edit, configure, and delete your Google Cloud Platform data. */
+  /** See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.. */
   public static final String CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
 
   /** Write Trace data for a project or application. */

8f0f6f202fe29394118c2f6af9ddd6a7f6a5570a
yoshi-automation <yoshi-automation@google.com>
2021-10-06 04:01:44 -0700

    Regenerate run client

--- clients/google-api-services-run/v1/1.31.0/com/google/api/services/run/v1/model/ConfigMapVolumeSource.java
+++ clients/google-api-services-run/v1/1.31.0/com/google/api/services/run/v1/model/ConfigMapVolumeSource.java
@@ -32,10 +32,15 @@ package com.google.api.services.run.v1.model;
 public final class ConfigMapVolumeSource extends com.google.api.client.json.GenericJson {
 
   /**
-   * (Optional) Mode bits to use on created files by default. Must be a value between 0 and 0777.
-   * Defaults to 0644. Directories within the path are not affected by this setting. This might be
-   * in conflict with other options that affect the file mode, like fsGroup, and the result can be
-   * other mode bits set.
+   * (Optional) Integer representation of mode bits to use on created files by default. Must be a
+   * value between 01 and 0777 (octal). If 0 or not set, it will default to 0644. Directories within
+   * the path are not affected by this setting. Notes * Internally, a umask of 0222 will be applied
+   * to any non-zero value. * This is an integer representation of the mode bits. So, the octal
+   * integer value should look exactly as the chmod numeric notation with a leading zero. Some
+   * examples: for chmod 777 (a=rwx), set to 0777 (octal) or 511 (base-10). For chmod 640
+   * (u=rw,g=r), set to 0640 (octal) or 416 (base-10). For chmod 755 (u=rwx,g=rx,o=rx), set to 0755
+   * (octal) or 493 (base-10). * This might be in conflict with other options that affect the file
+   * mode, like fsGroup, and the result can be other mode bits set.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
@@ -67,10 +72,15 @@ public final class ConfigMapVolumeSource extends com.google.api.client.json.Gene
   private java.lang.Boolean optional;
 
   /**
-   * (Optional) Mode bits to use on created files by default. Must be a value between 0 and 0777.
-   * Defaults to 0644. Directories within the path are not affected by this setting. This might be
-   * in conflict with other options that affect the file mode, like fsGroup, and the result can be
-   * other mode bits set.
+   * (Optional) Integer representation of mode bits to use on created files by default. Must be a
+   * value between 01 and 0777 (octal). If 0 or not set, it will default to 0644. Directories within
+   * the path are not affected by this setting. Notes * Internally, a umask of 0222 will be applied
+   * to any non-zero value. * This is an integer representation of the mode bits. So, the octal
+   * integer value should look exactly as the chmod numeric notation with a leading zero. Some
+   * examples: for chmod 777 (a=rwx), set to 0777 (octal) or 511 (base-10). For chmod 640
+   * (u=rw,g=r), set to 0640 (octal) or 416 (base-10). For chmod 755 (u=rwx,g=rx,o=rx), set to 0755
+   * (octal) or 493 (base-10). * This might be in conflict with other options that affect the file
+   * mode, like fsGroup, and the result can be other mode bits set.
    * @return value or {@code null} for none
    */
   public java.lang.Integer getDefaultMode() {
@@ -78,10 +88,15 @@ public final class ConfigMapVolumeSource extends com.google.api.client.json.Gene
   }
 
   /**
-   * (Optional) Mode bits to use on created files by default. Must be a value between 0 and 0777.
-   * Defaults to 0644. Directories within the path are not affected by this setting. This might be
-   * in conflict with other options that affect the file mode, like fsGroup, and the result can be
-   * other mode bits set.
+   * (Optional) Integer representation of mode bits to use on created files by default. Must be a
+   * value between 01 and 0777 (octal). If 0 or not set, it will default to 0644. Directories within
+   * the path are not affected by this setting. Notes * Internally, a umask of 0222 will be applied
+   * to any non-zero value. * This is an integer representation of the mode bits. So, the octal
+   * integer value should look exactly as the chmod numeric notation with a leading zero. Some
+   * examples: for chmod 777 (a=rwx), set to 0777 (octal) or 511 (base-10). For chmod 640
+   * (u=rw,g=r), set to 0640 (octal) or 416 (base-10). For chmod 755 (u=rwx,g=rx,o=rx), set to 0755
+   * (octal) or 493 (base-10). * This might be in conflict with other options that affect the file
+   * mode, like fsGroup, and the result can be other mode bits set.
    * @param defaultMode defaultMode or {@code null} for none
    */
   public ConfigMapVolumeSource setDefaultMode(java.lang.Integer defaultMode) {

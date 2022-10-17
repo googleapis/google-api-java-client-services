607c742a649d3556c6994e1f4eed6d1521fcf3bb
Mridula Peddada <mpeddada@google.com>
2022-10-17 17:49:17 -0400

    add print statements

--- .kokoro/generator_test.sh
+++ .kokoro/generator_test.sh
@@ -17,8 +17,11 @@ EXIT_STATUS=0
 
 pushd $(dirname "$0")/..
 python2 -m pip install --no-deps -e generator/ --user
+
+echo "AFTER DOWNLOADING PROJECT"
 python2 -m pip install --require-hashes -r .kokoro/test_requirements.txt
 
+echo "AFTER DOWNLOADING DEPS"
 pushd generator
 for file in `find tests/ -name '*_test.py'`
 do

5466afba72cffaf2720b5777f1b73faaf0bf11cb
Diego Marquez <diegomarquezp@google.com>
2023-08-08 19:47:04 +0000

    chore: fix python2 in generate.sh

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -32,8 +32,8 @@ then
 fi
 
 # Install the local generator without dependencies first and then install the dependencies with hash checking.
-python2 -m pip install --no-deps -e ${ROOT_DIR}/google-api-java-client-services/generator --user -q
-python2 -m pip install --require-hashes -r ${ROOT_DIR}/google-api-java-client-services/generator/generator_requirements.txt --user
+python -m pip install --no-deps -e ${ROOT_DIR}/google-api-java-client-services/generator --user -q
+python -m pip install --require-hashes -r ${ROOT_DIR}/google-api-java-client-services/generator/generator_requirements.txt --user
 
 pushd ${ROOT_DIR}/discovery-artifact-manager
 

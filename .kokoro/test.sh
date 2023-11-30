3083998712b417afce6c5cf00c2a536b0f54a8db
Lawrence Qiu <lawrenceqiu@google.com>
2023-11-30 15:37:05 +0000

    ci: Fix test CI job to check directory from root (#18922)
    * ci: Fix test CI job to check directory from root
    
    * ci: Fix wording

--- .kokoro/test.sh
+++ .kokoro/test.sh
@@ -23,21 +23,18 @@ git config --global --add safe.directory $(realpath .)
 
 VARIANT="2.0.0"
 
-# Only search for directories with the latest variant as only the generator only
+# Only search for directories with the latest variant as the generator only
 # generates libraries for the latest variant
 for directory in `find clients -mindepth 3 -maxdepth 3 -type d | grep ${VARIANT} | sort`
 do
-  pushd $directory
-
   # Find any diffs in the PR branch that are in this directory
-  diff=$(git diff "${KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH}...${KOKORO_GITHUB_PULL_REQUEST_COMMIT}" -- "${directory}")
-  if [ -z "$diff" ]; then
+  diff=$(git diff --name-only "${KOKORO_GITHUB_PULL_REQUEST_TARGET_BRANCH}...${KOKORO_GITHUB_PULL_REQUEST_COMMIT}" -- "${directory}")
+  if [ -z "${diff}" ]; then
     # Skip compilation + Running tests
     echo "No differences found in the PR branch for ${directory}, skipping..."
   else
+    echo "Found differences in ${directory}. Compiling..."
     mvn clean verify package -Dclirr.skip=true -Dmaven.javadoc.skip=true -B
   fi
-
-  popd
 done
 popd

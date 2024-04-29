8f5f2387c18e1b0a26f0bd2430d584339e4020c3
diegomarquezp <diegomarquezp@google.com>
2024-04-29 17:42:29 +0000

    test: add verify compilation script

---
+++ .github/workflows/verify_compilation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script loops over the generated clients and uses `mvn compile` to
+# create a list of clients that cannot be compiled. The resulting list is
+# printed to stdout
+set -exo
+export repo_root=$(git rev-parse --show-toplevel)
+export failed_libs="${repo_root}/failed_libs"
+
+process_client() {
+  pom=$1
+  pom_dir=$(dirname "${pom}")
+  pushd "${pom_dir}"
+  lib_name=$(pwd | sed 's/.*\(google-api-services-.*\)/\1/')
+  mvn compile || echo "${lib_name}" >> "${failed_libs}"
+  popd #pom_dir
+}
+
+print_failed_libraries() {
+  if [[ $(cat "${failed_libs}" | wc -l) -ne 0 ]]; then
+    echo "The following libraries had compilation errors:"
+    cat "${failed_libs}"
+    exit 1
+  else
+    echo "All libraries were compiled successfully!"
+  fi
+}
+
+# export process_client so it can be accessed by the inner shell launched by the
+# find command
+export -f print_failed_libraries
+pushd "${repo_root}"
+find . -name 'pom.xml' -exec bash -c 'process_client "$0"' {} \;
+print_failed_libraries
+
+
+popd #repo_root

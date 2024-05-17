d21edeb25c00e232090c1e1d450f3956941a38dc
Tomo Suzuki <suztomo@google.com>
2024-05-17 15:49:34 -0400

    python3 version

--- .kokoro/generator_test.sh
+++ .kokoro/generator_test.sh
@@ -18,7 +18,9 @@ EXIT_STATUS=0
 pushd $(dirname "$0")/..
 
 # Install the generator directory without dependencies first and then install the dependencies with hash checking.
-python3 -m pip install --upgrade pip
+echo "using $(python3 --version)"
+curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py
+python3 get-pip.py
 python3 -m pip install --no-deps -e generator/ --user
 python3 -m pip install --require-hashes -r generator/generator_requirements.txt --user
 

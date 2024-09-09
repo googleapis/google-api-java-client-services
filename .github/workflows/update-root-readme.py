21163c28e8218df704a02f869cf0dfd606b9aa25
Diego Marquez <diegomarquezp@google.com>
2024-09-09 09:46:28 -0400

    chore: restore readme generation (#23092)
    This introduces an excerpt the logic of an old version of the [now removed synth.py file](https://togithub.com/googleapis/google-api-java-client-services/blob/83926ff53607d212a865beb842465ca57b7cb55f/synth.py) in order to re-generate the root-level README.md.
    
    Since the other workflows still clone discovery-artifact-manager, this one does the same.
    It introduces requirements files for the python script as well.
    
    Field tested in https://togithub.com/googleapis/google-api-java-client-services/pull/23093 - see [logs](https://togithub.com/googleapis/google-api-java-client-services/actions/runs/10745179395/job/29803689929?pr=23092)
    
    __Before merging:__
     - [ ] remove the `on.pull_request` trigger in the workflow

---
+++ .github/workflows/update-root-readme.py
@@ -0,0 +1,149 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+from os import path, remove, getcwd
+from pathlib import Path
+import glob
+import re
+import sys
+from typing import List
+from absl import app
+import json
+from git import Repo
+import shutil
+
+
+# global vars
+VERSION_REGEX = r"([^\.]*)\.(.+)\.json$"
+SCRIPT_DIR = Path(path.dirname(path.realpath(__file__)))
+REPO_DIR = Path(str(SCRIPT_DIR / '..' / '..'))
+DISCOVERY_REPO_PATH = Path(path.abspath(path.join(getcwd(), './discovery-artifact-manager')))
+_discovery_repo = None
+
+
+def remove_discovery_repo():
+    if path.isdir(DISCOVERY_REPO_PATH):
+        shutil.rmtree(DISCOVERY_REPO_PATH)
+
+def clone_discovery_repo():
+    global _discovery_repo
+    # remove previously downloaded repo if exists
+    remove_discovery_repo()
+    discovery_repo_url = "https://github.com/googleapis/discovery-artifact-manager.git"
+    print(f"Cloning {discovery_repo_url}.")
+    # we clone with depth 1 because we only need the latest commit contents without history
+    _discovery_repo = Repo.clone_from(discovery_repo_url, DISCOVERY_REPO_PATH, depth=1)
+
+
+class Service:
+    id: str = None
+    title: str = None
+    version: str = None
+
+    def __init__(self, discovery_path: str):
+        match = re.match(VERSION_REGEX, path.basename(discovery_path))
+        if match is not None:
+            self.id = match[1]
+            self.version = match[2]
+
+            with open(discovery_path, "r") as f:
+                data = json.load(f)
+                self.title = data["title"]
+        else:
+            print(path.basename(discovery_path))
+
+
+def service_row(services: List[Service]) -> str:
+    services = sorted(services, key=lambda service: service.version)
+    links = [
+        f"[{service.version}](clients/google-api-services-{service.id}/{service.version})"
+        for service in services
+    ]
+    link = ", ".join(links)
+    name = services[0].title
+    return f"| {name} | {link} |\n"
+
+
+def replace_content_in_readme(content_rows: List[str]) -> None:
+    START_MARKER = "[//]: # (API_TABLE_START)"
+    END_MARKER = "[//]: # (API_TABLE_END)"
+    newlines = []
+    repl_open = False
+    readme_path = str(REPO_DIR / 'README.md')
+    with open(readme_path, "r") as f:
+        for line in f:
+            if not repl_open:
+                newlines.append(line)
+
+            if line.startswith(START_MARKER):
+                repl_open = True
+                newlines = newlines + content_rows
+            elif line.startswith(END_MARKER):
+                newlines.append("\n")
+                newlines.append(line)
+                repl_open = False
+
+    with open(readme_path, "w") as f:
+        for line in newlines:
+            f.write(line)
+
+
+def generate_service_list(services: List[Service]) -> None:
+    services_by_name = {}
+    for service in services:
+        if service.title is None:
+            print(service.id)
+
+        if service.title not in services_by_name:
+            services_by_name[service.title] = []
+
+        services_by_name[service.title].append(service)
+
+    content_rows = [
+        "\n",
+        "| API | Versions |\n",
+        "| --- | -------- |\n",
+    ]
+
+    # print(services_by_name.keys())
+    content_rows += [
+        service_row(services_by_name[name]) for name in sorted(services_by_name.keys())
+    ]
+
+    replace_content_in_readme(content_rows)
+
+def all_services():
+    services = []
+    for file in glob.glob(str(DISCOVERY_REPO_PATH / "discoveries/*.*.json")):
+        match = re.match(VERSION_REGEX, path.basename(file))
+        service = Service(file)
+        services.append(service)
+
+    return services
+
+
+def main(argv: List[str]) -> None:
+    if len(argv) > 1:
+        print('update-root-readme.py takes no arguments')
+        exit(1)
+    clone_discovery_repo()
+    services = all_services()
+    generate_service_list(services)
+    remove_discovery_repo()
+
+
+if __name__ == "__main__":
+  app.run(main)

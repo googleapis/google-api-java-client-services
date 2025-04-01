10984cb32fb8e724cf9b31e8e6e35d6a95df7a40
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2025-04-01 11:02:43 -0400

    move to script file

---
+++ .github/workflows/verify.sh
@@ -0,0 +1,24 @@
+set -ex
+export repo_dir=$(realpath $(dirname "${BASH_SOURCE[0]}")/../../)
+pushd "${repo_dir}/clients"
+
+# find all generated clients' pom.xml
+find . -name pom.xml -not -path '*/target/*' > pom_list
+cat pom_list
+
+# format result to list of Maven modules
+cat pom_list | cut -d'/' -f2-4 \
+| sed 's/\(.*\)/<module>\1<\/module>/' > module_list
+cat module_list
+
+# produce a temporary pom with the modules
+(echo "<project><modelVersion>4.0.0</modelVersion><groupId>temp</groupId><artifactId>temp</artifactId><version>1.0</version><packaging>pom</packaging><modules>"; cat module_list; echo "</modules></project>") > pom.xml
+cat pom.xml
+
+# test compilation
+mvn clean compile -Dmaven.testSkip=true -fae --fail-at-end 2>&1 | tee out
+cat out | grep -E "^\[ERROR\] Failed to execute goal" > errors
+if [[ $(cat errors | wc -l) -gt 0 ]]; then
+	echo "Compilation errors found"
+	exit 1
+fi
